02284f32e514212b6f93dc367c077ec6bf373247
paul <paul.jose@confianzit.biz>
2019-12-10 15:52:25 +0530

    cron for clearing price lock

--- price_paper/models/product_pricelist.py
+++ price_paper/models/product_pricelist.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api, _
+from datetime import datetime
 
 
 class ProductPricelist(models.Model):
@@ -13,13 +14,25 @@ class ProductPricelist(models.Model):
     expiry_date = fields.Date('Valid Upto')
     price_lock = fields.Boolean(string='Price Change Lock', default=False)
     lock_expiry_date = fields.Date(string='Lock Expiry date')
-    partner_id = fields.Many2one('res.partner', string='Customer', store=True) #compute='_compute_partner', 
+    partner_id = fields.Many2one('res.partner', string='Customer', store=True) #compute='_compute_partner',
 
 #    @api.depends('customer_product_price_ids.partner_id')
 #    def _compute_partner(self):
 #        for pricelist in self:
 #            if pricelist.type == 'customer':
 #                pricelist.partner_id = pricelist.customer_product_price_ids and pricelist.customer_product_price_ids[0].partner_id.id or False
+
+    @api.model
+    def _cron_update_price_change_lock(self):
+        domain = [('price_lock', '=', True), ('lock_expiry_date', '<', datetime.today())]
+        pricelists = self.search(domain)
+        product_prices = self.env['customer.product.price'].search(domain)
+        vals = {
+            'price_lock': False,
+            'lock_expiry_date': False
+        }
+        pricelists.write(vals)
+        product_prices.write(vals)
 
 
 ProductPricelist()

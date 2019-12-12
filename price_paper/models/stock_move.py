ad2fd655826e88169a5d888fd844f24348e2bf1f
paul <paul.jose@confianzit.biz>
2019-12-12 16:36:59 +0530

    Stock move changes: One DO per one SO

--- price_paper/models/stock_move.py
+++ price_paper/models/stock_move.py
@@ -7,6 +7,19 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     picking_partner_id = fields.Many2one('res.partner', related='picking_id.partner_id', string='Partner')
+    
+    def _search_picking_for_assignation(self):
+        """
+        Overriden to create one DO per one SO.
+        """
+        self.ensure_one()
+        picking = self.env['stock.picking'].search([
+                ('group_id', '=', self.group_id.id),
+                ('location_dest_id', '=', self.location_dest_id.id),
+                ('picking_type_id', '=', self.picking_type_id.id),
+                ('printed', '=', False),
+                ('state', 'in', ['draft', 'confirmed', 'waiting', 'partially_available', 'assigned'])], limit=1)
+        return picking
 
 
 StockMove()
@@ -15,6 +28,8 @@ class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     picking_partner_id = fields.Many2one('res.partner', related='move_id.picking_partner_id', string='Partner')
+
+
 
 
 StockMoveLine()

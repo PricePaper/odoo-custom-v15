778147fb67daad5498b837fb1f9cd89271759775
paul <paul.jose@confianzit.biz>
2020-02-05 16:37:20 +0530

    quick sale changes

--- instant_invoice/models/sale.py
+++ instant_invoice/models/sale.py
@@ -4,9 +4,12 @@ from odoo import models, fields, api, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    quick_sale = fields.Boolean(string='is_quick_sale', default=False)
+
     @api.multi
     def action_quick_sale(self):
         for rec in self:
+            # rec.quick_sale = True
             rec.action_confirm()
             rec.picking_ids.action_confirm()
             rec.picking_ids.action_assign()
@@ -17,6 +20,7 @@ class SaleOrder(models.Model):
             rec.picking_ids.deliver_products()
             rec.action_invoice_create()
             rec.invoice_ids.action_invoice_open()
+            rec.quick_sale = True
         return True
 
     @api.multi
@@ -28,5 +32,11 @@ class SaleOrder(models.Model):
     def action_print_picking_operation(self):
         picking = self.picking_ids
         return picking.print_picking_operation()
+
+    @api.multi
+    def action_print_product_label(self):
+        picking = self.picking_ids
+        return picking.print_product_label()
+
 
 SaleOrder()
--- instant_invoice/models/stock_picking.py
+++ instant_invoice/models/stock_picking.py
@@ -9,3 +9,7 @@ class StockPicking(models.Model):
     @api.multi
     def print_picking_operation(self):
         return self.env.ref('stock.action_report_picking').report_action(self, config=False)
+
+    @api.multi
+    def print_product_label(self):
+        return self.env.ref('batch_delivery.product_label_report').report_action(self, config=False)

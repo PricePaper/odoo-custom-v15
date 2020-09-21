02f7b7d71398fb20aaf736e98eeec1641af831a0
paul <paul.jose@confianzit.biz>
2020-09-21 14:12:15 +0530

    truck route sorting

--- batch_delivery/models/truck_route.py
+++ batch_delivery/models/truck_route.py
@@ -8,6 +8,7 @@ class TruckRoute(models.Model):
 
     _name = 'truck.route'
     _description = 'Truck Route'
+    _order = 'name'
 
     name = fields.Char(string='Name')
     set_active = fields.Boolean(string='Set Active')

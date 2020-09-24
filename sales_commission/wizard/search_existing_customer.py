eea89f9683f05a86e82bdc0a74b0d6c9ea595a4a
paul <paul.jose@confianzit.biz>
2020-09-24 09:39:07 +0530

    parent field in search tree view

--- sales_commission/wizard/search_existing_customer.py
+++ sales_commission/wizard/search_existing_customer.py
@@ -17,7 +17,7 @@ class SearchExistingCustomer(models.TransientModel):
         self.line_ids = []
         if self.search_string != False:
             string = '%'+self.search_string+'%'
-            query = """SELECT name, commercial_company_name, email, phone, mobile, vat, last_so, last_so_date from res_partner where (name ilike('%s') or phone ilike('%s') or mobile ilike('%s') or email ilike('%s') or commercial_company_name ilike('%s') or vat ilike('%s')) and customer=True;""" % (string, string, string, string, string, string)
+            query = """SELECT name, commercial_company_name, email, phone, mobile, vat, last_so, last_so_date, parent_id from res_partner where (name ilike('%s') or phone ilike('%s') or mobile ilike('%s') or email ilike('%s') or commercial_company_name ilike('%s') or vat ilike('%s')) and customer=True;""" % (string, string, string, string, string, string)
             self.env.cr.execute(query)
             result = self.env.cr.fetchall()
             if result:
@@ -31,6 +31,7 @@ class SearchExistingCustomer(models.TransientModel):
                            'last_so': rec[6],
                            'last_so_date': rec[7],
                            'parent_id': self.id,
+                           'parent_partner': rec[8],
                            }
                     res.append((0, _, val))
                 self.line_ids = res
@@ -56,5 +57,6 @@ class ExistingCustomerLine(models.TransientModel):
     last_so = fields.Char(string='Last Sale order')
     last_so_date = fields.Datetime(string='Last Sale Date')
     parent_id = fields.Many2one('search.existing.customer', string='Parent')
+    parent_partner = fields.Many2one('res.partner', string='Parent')
 
 ExistingCustomerLine()

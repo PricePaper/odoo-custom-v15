00b3b8f3c2bae8a183333155066490c06b745875
paul <paul.jose@confianzit.biz>
2020-10-13 18:27:41 +0530

    default delay in company

--- stock_orderpoint_enhancements/models/res_company.py
+++ stock_orderpoint_enhancements/models/res_company.py
@@ -6,7 +6,7 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     delay = fields.Integer(string='Delivery Lead Time',
-        default=15,
+        default=21,
         help="Lead time in days between the confirmation of the purchase order and the receipt of the products in your warehouse.")
     buffer_percetage = fields.Float(string='OrderPoint Buffer percentage')
     order_freq = fields.Integer(string='Order Frequency', default=30)

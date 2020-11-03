7d92470aad4fb488ab96f4d9785d698da1e49f4d
paul <paul.jose@confianzit.biz>
2020-11-03 15:11:13 +0530

    customer price maintanace missing chnage

--- price_paper/models/customer_pricelist.py
+++ price_paper/models/customer_pricelist.py
@@ -6,10 +6,12 @@ from odoo import fields, models, api
 class CustomerPricelist(models.Model):
     _name = 'customer.pricelist'
     _description = 'Customer Pricelist Link'
+    _rec_name = 'pricelist_id'
 
 
     sequence = fields.Integer(string='Sequence')
     pricelist_id = fields.Many2one('product.pricelist', string='Pricelist')
     partner_id = fields.Many2one('res.partner', string='Customer')
+
 
 CustomerPricelist()

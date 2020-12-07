9b7dfd850284fe520ddc86c1aed27c65f96417b3
Aswanth <aswanth.babu@confianzit.biz>
2020-12-07 13:50:38 +0530

    [ADD] Reset button in route assign view.

---
+++ batch_delivery/static/src/js/kanban_reset_button.js
@@ -0,0 +1,49 @@
+odoo.define('batch_delivery.kanban_reset_button', function (require) {
+
+
+var core = require('web.core');
+var KanbanController = require('web.KanbanController');
+var KanbanView = require('web.KanbanView');
+var viewRegistry = require('web.view_registry');
+var framework = require('web.framework');
+
+var qweb = core.qweb;
+
+
+var KanbanButtonController = KanbanController.extend({
+	init: function () {
+    	this._super.apply(this, arguments);
+    },
+
+    renderButtons: function ($node) {
+        console.log('hello');
+        this.$buttons = $(qweb.render('KanbanReset.buttons'));
+        this.$buttons.on('click', '.o_kanban_discard', this._onKanbanReset.bind(this));
+        this.$buttons.appendTo($node);
+
+    },
+
+    _onKanbanReset: function () {
+        framework.blockUI();
+        var self = this;
+        console.log('hello',this);
+        this._rpc({
+            model: 'stock.picking',
+            method: 'reset_picking_with_route',
+            args: [this.initialState.res_ids],
+        }).then(function () {
+            self.reload();
+        }).always(framework.unblockUI.bind(framework));
+    },
+});
+
+var KanbanButtonView = KanbanView.extend({
+    config: _.extend({}, KanbanView.prototype.config, {
+        Controller: KanbanButtonController,
+    }),
+});
+
+viewRegistry.add('kanban_reset', KanbanButtonView);
+
+});
+

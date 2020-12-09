e98b3ff942ca386620a77d7bfe09e4defd628bd2
Aswanth <aswanth.babu@confianzit.biz>
2020-12-09 22:20:38 +0530

    [Update] UI Enhancement Reloading optimized!!.

--- batch_delivery/static/src/js/kanban_reset_button.js
+++ batch_delivery/static/src/js/kanban_reset_button.js
@@ -1,47 +1,45 @@
 odoo.define('batch_delivery.kanban_reset_button', function (require) {
 
-
-var core = require('web.core');
-var KanbanController = require('web.KanbanController');
-var KanbanView = require('web.KanbanView');
-var viewRegistry = require('web.view_registry');
-var framework = require('web.framework');
-
-var qweb = core.qweb;
+    var KanbanController = require('web.KanbanController');
+    var KanbanView = require('web.KanbanView');
+    var viewRegistry = require('web.view_registry');
+    var framework = require('web.framework');
 
 
-var KanbanButtonController = KanbanController.extend({
-	init: function () {
-    	this._super.apply(this, arguments);
-    },
+    var KanbanButtonController = KanbanController.extend({
 
-    renderButtons: function ($node) {
-        this.$buttons = $(qweb.render('KanbanReset.buttons'));
-        this.$buttons.on('click', '.o_kanban_discard', this._onKanbanReset.bind(this));
-        this.$buttons.appendTo($node);
+        renderButtons: function ($node) {
+            this._super.apply(this, arguments);
+            this.$buttons = $('<button/>', {
+                class: ['btn btn-primary o_kanban_discard']
+                }).text(_('RESET')).on('click', this._onKanbanReset.bind(this)).appendTo($node);
+        },
 
-    },
+        _onKanbanReset: function () {
+            framework.blockUI();
+            var self = this;
+            this._rpc({
+                model: 'stock.picking',
+                method: 'reset_picking_with_route',
+                args: [this.initialState.res_ids],
+            }).then(function () {
+                self.reload();
+            }).always(framework.unblockUI.bind(framework));
+        },
+    });
 
-    _onKanbanReset: function () {
-        framework.blockUI();
-        var self = this;
-        this._rpc({
-            model: 'stock.picking',
-            method: 'reset_picking_with_route',
-            args: [this.initialState.res_ids],
-        }).then(function () {
-            self.reload();
-        }).always(framework.unblockUI.bind(framework));
-    },
-});
+    var KanbanButtonView = KanbanView.extend({
+        config: _.extend({}, KanbanView.prototype.config, {
+            Controller: KanbanButtonController,
+        }),
+    });
 
-var KanbanButtonView = KanbanView.extend({
-    config: _.extend({}, KanbanView.prototype.config, {
-        Controller: KanbanButtonController,
-    }),
-});
+    viewRegistry.add('kanban_reset', KanbanButtonView);
 
-viewRegistry.add('kanban_reset', KanbanButtonView);
+    return {
+        KanbanButtonController: KanbanButtonController,
+        KanbanButtonView: KanbanButtonView,       
+    };
 
 });
 

5028aad4f465a3f80858611cfdbbf65aecb14143
Aswanth <aswanth.babu@confianzit.biz>
2020-12-17 18:31:20 +0530

    [ADDED] addons product logic added

--- price_paper/wizard/add_purchase_history_to_so.py
+++ price_paper/wizard/add_purchase_history_to_so.py
@@ -194,7 +194,6 @@ class AddPurchaseHistorySO(models.TransientModel):
                     'product_id': line_id.order_line.product_id.id,
                     'product_uom': line_id.order_line.product_uom.id,
                     'product_uom_qty': line_id.qty_to_be,
-                    'qty_available': line_id.qty_available,
                     'price_unit': price,
                     'order_id': order_id and order_id.id or False,
                     'lst_price': line_id.order_line.product_id.lst_price,

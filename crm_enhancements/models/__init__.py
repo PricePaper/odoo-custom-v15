6d29f61c1200fdc1a9d261a051900262738f55ae
Aswanth <aswanth.babu@confianzit.biz>
2020-12-29 10:51:08 +0530

    [CODE CLEANUP] crm_enhancements

--- crm_enhancements/models/__init__.py
+++ crm_enhancements/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from . import crm_lead
 from . import res_company
 from . import res_partner
--- crm_enhancements/wizard/__init__.py
+++ crm_enhancements/wizard/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from . import crm_lead_to_oppertunity
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

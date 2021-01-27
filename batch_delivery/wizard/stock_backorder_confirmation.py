4dcd4a1f833e6522b8afaa56feb4e84aaa6b4721
paul <paul.jose@confianzit.biz>
2021-01-27 10:20:05 +0530

    purchase return bug fix

--- batch_delivery/wizard/stock_backorder_confirmation.py
+++ batch_delivery/wizard/stock_backorder_confirmation.py
@@ -12,29 +12,29 @@ class StockBackorderConfirmation(models.TransientModel):
     def _process(self, cancel_backorder=False):
         StockReturn = self.env['stock.picking.return']
         for pick_id in self.pick_ids:
-            StockReturn.create({
-                'name': 'RETURN-' + pick_id.name,
-                'reason': self.reason,
-                'picking_id': pick_id.id,
-                'sale_id': pick_id.sale_id.id,
-                'sales_person_ids': [
-                    (6, 0, pick_id.sale_id and
-                     pick_id.sale_id.sales_person_ids and
-                     pick_id.sale_id.sales_person_ids.ids)],
-                'return_line_ids': [(0, 0, {
-                    'product_id': move.product_id.id,
-                    'ordered_qty': move.product_uom_qty,
-                    'delivered_qty': move.quantity_done if move.reserved_availability == move.product_uom_qty else move.reserved_availability,
-                }) for move in pick_id.move_ids_without_package if move.quantity_done != move.product_uom_qty]
-            })
+            if pick_id.sale_id:
+                StockReturn.create({
+                    'name': 'RETURN-' + pick_id.name,
+                    'reason': self.reason,
+                    'picking_id': pick_id.id,
+                    'sale_id': pick_id.sale_id.id,
+                    'sales_person_ids': [
+                        (6, 0, pick_id.sale_id and
+                         pick_id.sale_id.sales_person_ids and
+                         pick_id.sale_id.sales_person_ids.ids)],
+                    'return_line_ids': [(0, 0, {
+                        'product_id': move.product_id.id,
+                        'ordered_qty': move.product_uom_qty,
+                        'delivered_qty': move.quantity_done if move.reserved_availability == move.product_uom_qty else move.reserved_availability,
+                    }) for move in pick_id.move_ids_without_package if move.quantity_done != move.product_uom_qty]
+                })
 
-            for move in pick_id.move_ids_without_package:
-                move.sale_line_id.pre_delivered_qty += move.quantity_done
-
-            if not cancel_backorder:
-                order = self.env['sale.order.line'].search(
-                    [('order_id', '=', pick_id.sale_id.id), ('is_delivery', '=', True)])
-                order.write({'product_uom_qty': order.product_uom_qty + 1})
+                for move in pick_id.move_ids_without_package:
+                    move.sale_line_id.pre_delivered_qty += move.quantity_done
+                if not cancel_backorder:
+                    order = self.env['sale.order.line'].search(
+                        [('order_id', '=', pick_id.sale_id.id), ('is_delivery', '=', True)])
+                    order.write({'product_uom_qty': order.product_uom_qty + 1})
 
         super(StockBackorderConfirmation, self)._process(cancel_backorder)
 
@@ -42,4 +42,3 @@ class StockBackorderConfirmation(models.TransientModel):
 StockBackorderConfirmation()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-

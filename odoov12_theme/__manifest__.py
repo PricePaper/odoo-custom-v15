3de2090da2663cbcd64e3336a3b5499fc5f3d713
paul <paul.jose@confianzit.biz>
2021-04-16 18:09:29 +0530

    Odoo theme- BG colour

---
+++ odoov12_theme/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Confianz IT
+#    Copyright (C) 2019   (https://www.confianzit.com)
+#
+##############################################################################
+
+
+{
+    'name': 'Confianz Theme',
+    'version': '12.0.1.0',
+    'category': 'Theme',
+    'sequence': '3',
+    'description': """
+        New theme for Odoo.
+    """,
+    'author': 'Confianz IT',
+    'website': 'https://www.confianzit.com',
+    'depends': ['web_enterprise'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

885a2b7d71cc9638dd1624ab4400c5e22e8b9e91
sajith <sajith@confianzit.biz>
2021-04-28 17:00:31 +0530

    code fix

--- stock_product_location/models/purchase.py
+++ stock_product_location/models/purchase.py
@@ -14,11 +14,13 @@ class PurchaseOrderLine(models.Model):
         res = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
         product_loc_id = self.product_id.property_stock_location.id or self.product_id.categ_id.property_stock_location.id or False
         if product_loc_id:
-            res[0].update({'location_dest_id': product_loc_id})
-
+            for val in res:
+                val['location_dest_id'] = product_loc_id
+#            res[0].update({'location_dest_id': product_loc_id})
         return res
 
 
 PurchaseOrderLine()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+

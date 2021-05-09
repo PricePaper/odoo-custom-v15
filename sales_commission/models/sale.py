4a9d5d30407dcaec329c9b17f56eac54d3e9b137
paul <paul.jose@confianzit.biz>
2021-05-09 21:59:37 +0530

    sale order line record rule

--- sales_commission/models/sale.py
+++ sales_commission/models/sale.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
@@ -11,5 +11,16 @@ class SaleOrder(models.Model):
 
 
 SaleOrder()
+
+class SaleOrder_line(models.Model):
+    _inherit = 'sale.order.line'
+
+    sales_person_ids = fields.Many2many('res.partner',  compute='get_sales_persons',
+                                        string='Associated Sales Persons')
+    @api.depends('order_id.partner_id', 'order_partner_id', 'order_partner_id.sales_person_ids')
+    def get_sales_persons(self):
+    	for rec in self:    		
+    		rec.sales_person_ids = [(6, 0, rec.order_partner_id.sales_person_ids.ids)]
+SaleOrder_line()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

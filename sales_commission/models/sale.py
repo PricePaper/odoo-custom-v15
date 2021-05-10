254be75f90319a9c19b3bb95b2977b1d7cca1167
paul <paul.jose@confianzit.biz>
2021-05-10 16:26:02 +0530

    sale order line rule fix

--- sales_commission/models/sale.py
+++ sales_commission/models/sale.py
@@ -15,12 +15,22 @@ SaleOrder()
 class SaleOrder_line(models.Model):
     _inherit = 'sale.order.line'
 
-    sales_person_ids = fields.Many2many('res.partner',  compute='get_sales_persons',
-                                        string='Associated Sales Persons')
+    sales_person_ids = fields.Many2many('res.partner',  compute='get_sales_persons', string='Associated Sales Persons', search='search_sales_persons')
+
+
     @api.depends('order_id.partner_id', 'order_partner_id', 'order_partner_id.sales_person_ids')
     def get_sales_persons(self):
-    	for rec in self:    		
-    		rec.sales_person_ids = [(6, 0, rec.order_partner_id.sales_person_ids.ids)]
+        for rec in self:
+            rec.sales_person_ids = [(6, 0, rec.order_partner_id.sales_person_ids.ids)]
+
+
+    @api.multi
+    def search_sales_persons(self, operator, value):
+        commission = self.env['commission.percentage'].search([('sale_person_id', operator, value)])
+        partner = commission.mapped('partner_id')
+        order = self.env['sale.order']._search([('partner_id', 'in', partner.ids)])
+        return[('order_id', 'in', order)]
+
 SaleOrder_line()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

f48438442f3884e7045496d85b314ac964e945f7
paul <paul.jose@confianzit.biz>
2021-06-12 11:09:16 +0530

    reset words in printing

--- batch_delivery/models/account_payment.py
+++ batch_delivery/models/account_payment.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     batch_id = fields.Many2one('stock.picking.batch', string='Delivery Batch')
+
+    @api.multi
+    def print_checks(self):
+        self.set_check_amount_in_words()
+        return super(AccountPayment, self).print_checks()
 
 
 AccountPayment()

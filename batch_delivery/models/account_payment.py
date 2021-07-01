2b027c5486b0ca61e6886d83e5d4ecebd50d512a
Aswanth <aswanth.babu@confianzit.biz>
2021-07-01 11:46:58 +0530

    [UPDATE] Track visibility for payment state field.

--- batch_delivery/models/account_payment.py
+++ batch_delivery/models/account_payment.py
@@ -7,10 +7,13 @@ class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     batch_id = fields.Many2one('stock.picking.batch', string='Delivery Batch')
+    state = fields.Selection([('draft', 'Draft'), ('posted', 'Posted'), ('sent', 'Sent'), ('reconciled', 'Reconciled'),
+                              ('cancelled', 'Cancelled')], readonly=True, default='draft', copy=False, string="Status", track_visibility='onchange')
 
     @api.multi
     def print_checks(self):
         self.set_check_amount_in_words()
+        self.state
         return super(AccountPayment, self).print_checks()
 
 

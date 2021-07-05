78fe54b7f4177a8341a2a3e15c68229e6a3dae74
paul <paul.jose@confianzit.biz>
2021-07-05 09:50:53 +0530

    Low Stock Warning Fix

--- price_paper/models/sale.py
+++ price_paper/models/sale.py
@@ -921,17 +921,17 @@ class SaleOrderLine(models.Model):
                 lang=self.order_id.partner_id.lang or self.env.user.lang or 'en_US'
             )
             product_qty = self.product_uom._compute_quantity(self.product_uom_qty, self.product_id.uom_id)
-            if float_compare(product.qty_available, product_qty, precision_digits=precision) == -1:
+            if float_compare(product.qty_available - product.outgoing_qty, product_qty, precision_digits=precision) == -1:
                 is_available = self._check_routing()
                 if not is_available:
                     message =  _('You plan to sell %s %s of %s but you only have %s %s available in %s warehouse.') % \
-                            (self.product_uom_qty, self.product_uom.name, self.product_id.name, product.qty_available, product.uom_id.name, self.order_id.warehouse_id.name)
+                            (self.product_uom_qty, self.product_uom.name, self.product_id.name, product.qty_available - product.outgoing_qty, product.uom_id.name, self.order_id.warehouse_id.name)
                     # We check if some products are available in other warehouses.
-                    if float_compare(product.qty_available, self.product_id.qty_available, precision_digits=precision) == -1:
+                    if float_compare(product.qty_available - product.outgoing_qty, self.product_id.qty_available - self.product_id.outgoing_qty, precision_digits=precision) == -1:
                         message += _('\nThere are %s %s available across all warehouses.\n\n') % \
-                                (self.product_id.qty_available, product.uom_id.name)
+                                (self.product_id.qty_available - self.product_id.outgoing_qty, product.uom_id.name)
                         for warehouse in self.env['stock.warehouse'].search([]):
-                            quantity = self.product_id.with_context(warehouse=warehouse.id).qty_available
+                            quantity = self.product_id.with_context(warehouse=warehouse.id).qty_available - self.product_id.with_context(warehouse=warehouse.id).outgoing_qty
                             if quantity > 0:
                                 message += "%s: %s %s\n" % (warehouse.name, quantity, self.product_id.uom_id.name)
                     warning_mess = {

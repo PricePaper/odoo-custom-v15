b66cef99648bbfd08ec25522495d26c4d470a812
Aswanth <aswanth.babu@confianzit.biz>
2021-07-12 22:24:29 +0530

    [ADD] (account_batch_payment_extension) add new related field in account move line

--- account_batch_payment_extension/models/account_invoice.py
+++ account_batch_payment_extension/models/account_invoice.py
@@ -84,4 +84,10 @@ class Accountinvoice(models.Model):
 
 Accountinvoice()
 
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    st_date = fields.Date(related='statement_line_id.date', readonly=True, store=True)
+
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

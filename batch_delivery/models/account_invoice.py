d94d49ddd4dccc80c59a0ba526a924d8c8deca5d
paul <paul.jose@confianzit.biz>
2021-07-14 14:01:27 +0530

    Flow change for avoid create new invoice after DO qty change

--- batch_delivery/models/account_invoice.py
+++ batch_delivery/models/account_invoice.py
@@ -67,6 +67,19 @@ class AccountInvoice(models.Model):
     def action_invoice_open(self):
 
         if self:
+            for invoice in self:
+                for line in invoice.invoice_line_ids:
+                    if line.quantity == 0:
+                        line.sudo().unlink()
+                delivery_inv_lines = self.env['account.invoice.line']
+                # if a invoice have only one line we need to make sure it's not a delivery charge.
+                # if its a devlivery change, remove it from invoice  and cancel the entry.
+
+                if len(invoice.invoice_line_ids) == 1:
+                    if all(invoice.invoice_line_ids.mapped('sale_line_ids').mapped('is_delivery')):
+                        delivery_inv_lines |= invoice.invoice_line_ids
+                if delivery_inv_lines:
+                    delivery_inv_lines.sudo().unlink()
             stock_picking = self.env['stock.picking']
             if  self.mapped('picking_ids').filtered(lambda pick: pick.state == 'cancel'):
                 raise UserError(_('There is a Cancelled Picking linked to this invoice.'))

5cd113b601f2fc8e29bee82fa9fd879995dc799c
Aswanth <aswanth.babu@confianzit.biz>
2021-07-22 19:20:28 +0530

    [UPDATE] change --> SC-PopUP Window from SO (button action).

--- price_paper/wizard/sc_popup_window.py
+++ price_paper/wizard/sc_popup_window.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api, _
 
 
 class StorageContractPopUpWindow(models.TransientModel):
@@ -7,14 +7,39 @@ class StorageContractPopUpWindow(models.TransientModel):
 
     partner_id = fields.Many2one('res.partner', string='Customer')
     contract_line_id = fields.Many2one('sale.order.line', string='Storage Contract')
+    order_qty = fields.Float(string='Order Quantity')
+
+    @api.onchange('contract_line_id')
+    def _onchange_contract_line(self):
+        self.order_qty = self.contract_line_id.storage_remaining_qty if self.contract_line_id.storage_remaining_qty < self.contract_line_id.selling_min_qty else self.contract_line_id.selling_min_qty
+
+    @api.onchange('order_qty')
+    def _onchange_order_qty(self):
+        res = {}
+        if self.order_qty < self.contract_line_id.selling_min_qty:
+            warning_mess = {
+                'title': _('Less than Minimum qty'),
+                'message': _('You are going to sell less than minimum qty in the contract.')
+            }
+            self.order_qty = 0
+            res.update({'warning': warning_mess})
+        elif self.order_qty > self.contract_line_id.storage_remaining_qty:
+            warning_mess = {
+                'title': _('More than Storage contract'),
+                'message': _(
+                    'You are going to Sell more than in storage contract.Only %s is remaining in this contract.' % (
+                        self.contract_line_id.storage_remaining_qty))
+            }
+            self.order_qty = 0
+            res.update({'warning': warning_mess})
+        return res
 
     def add_sc_line(self):
         sc_line = self.contract_line_id
-        qty = sc_line.storage_remaining_qty if sc_line.storage_remaining_qty < sc_line.selling_min_qty else sc_line.selling_min_qty
         line = self.env['sale.order.line'].create({
             'product_id': sc_line.product_id.id,
             'storage_contract_line_id': sc_line.id,
-            'product_uom_qty': qty,
+            'product_uom_qty': self.order_qty,
             'order_id': self._context.get('active_id')
         })
         line.product_id_change()

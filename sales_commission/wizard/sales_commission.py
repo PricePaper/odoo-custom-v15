5f27ebdfcaf2ce69f5de37ca09a6d72645502a25
paul <paul.jose@confianzit.biz>
2021-07-28 09:56:43 +0530

    sales commission Audit report

--- sales_commission/wizard/sales_commission.py
+++ sales_commission/wizard/sales_commission.py
@@ -1,40 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import calendar
 
 
 class GenerateCommission(models.TransientModel):
     _name = 'generate.sales.commission'
     _description = 'Generate Sales commission'
 
-    report_type = fields.Selection(
-        [('invoice_paid', 'Invoice Paid Orders'), ('payment_pending', 'Payment Pending Orders')],
-        string='Calculated For', default='invoice_paid', required=True)
-    salesperson_id = fields.Many2one('res.partner', string='Sales Persons', domain=[('is_sales_person', '=', True)])
+    @api.model
+    def _get_months(self):
+        res = []
+        startdate = datetime.now()
+        for i in range(0,12):
+            date = startdate - relativedelta(months=i)
+            key = format(date, '%m-%Y')
+            value = format(date, '%B %Y')
+            res.append((key, value))
+        return res
+
+    month = fields.Selection(string="Month", selection=_get_months, required=True)
 
     @api.multi
     def generate_commission(self):
-        self.ensure_one()
-        paid = False
+        return self.env.ref('sales_commission.commission_audit_report').report_action(self)
 
-        if self.report_type == 'invoice_paid':
-            paid = True
-        domain = [['is_paid', '=', paid]]
-        if self.salesperson_id:
-            domain.append(['sale_person_id', '=', self.salesperson_id.id])
-        view_id = self.env.ref('sales_commission.view_commission_sale_grouped_tree').id
-        res = {
-            "type": "ir.actions.act_window",
-            "name": "Sale commission",
-            "res_model": "sale.commission",
-            "views": [[view_id, "tree"]],
-            "context": {'default_search_is_paid': True, 'group_by': ['sale_person_id', 'write_date:month']},
-            "domain": domain,
-            "target": "current",
-        }
-        if not self.env.user.has_group('sales_commission.group_sales_commission'):
-            res['domain'].append(['sale_person_id', '=', self.env.user.partner_id.id])
-        return res
 
 
 GenerateCommission()

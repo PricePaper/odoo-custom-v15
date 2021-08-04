3e6f446541cd6538528dc58bb7382660be266629
Deviprasad <deviprasad@confianzit.biz>
2021-08-04 13:24:42 +0530

    script to correct imported SC

--- sales_commission/models/sale.py
+++ sales_commission/models/sale.py
@@ -9,6 +9,7 @@ class SaleOrder(models.Model):
     sales_person_ids = fields.Many2many('res.partner', related='partner_id.sales_person_ids',
                                         string='Associated Sales Persons')
     
+    @api.model
     def link_sc(self, sc_line_id=False, po_line_id=False):
         sc_line = self.env['sale.order.line'].browse(sc_line_id)
         po_line = self.env['purchase.order.line'].browse(po_line_id)
@@ -16,6 +17,59 @@ class SaleOrder(models.Model):
         po_line.move_ids.write({'sale_line_id': sc_line.id, 'is_storage_contract': True})
         po_line.invoice_lines.write({'is_storage_contract': True})
         sc_line.order_id.write({'state': 'released', 'sc_po_done': True})
+        return True
+
+    @api.model
+    def correct_sc(self, order_ids=[]):
+        if not order_ids:
+            return
+        sc_inv = 0        
+        for order in self.browse(order_ids):
+            line_ids = []
+            order.action_storage_contract_confirm()
+            for line in order.order_line:
+                if line.product_uom_qty > line.product_id.qty_available:
+                    line.product_uom_qty = line.product_id.qty_available
+                sc_inv = line.product_uom_qty * line.product_id.standard_price
+                line_ids.extend([[0,0, {                                
+                                'debit': sc_inv,
+                                'credit': 0,
+                                'journal_id': 3,
+                                'name': "Storage contract inventory transfer %s" % line.product_id.name,                                
+                                'account_id': 683,
+                                'date_maturity': "2021-4-30",
+                            }],
+                            [0,0, {                                
+                                'debit': 0,
+                                'credit': sc_inv,
+                                'journal_id': 3,
+                                'name': "Storage contract inventory transfer %s" % line.product_id.name,
+                                'account_id': 687,
+                                'date_maturity': "2021-4-30",
+                            }],[0,0, {                                
+                                'debit': 0,
+                                'credit': sc_inv,
+                                'journal_id': 3,
+                                'name': "Storage contract cost transfer %s" % line.product_id.name,
+                                'account_id': 769,
+                                'date_maturity': "2021-4-30",
+                            }],[0,0, {                                
+                                'debit': sc_inv,
+                                'credit': 0,
+                                'journal_id': 3,
+                                'name': "Storage contract cost transfer %s" % line.product_id.name,
+                                'account_id': 29,
+                                'date_maturity': "2021-4-30",
+                            }],])        
+            move_id = self.env['account.move'].create({
+                'ref': "Storage contract imported order %s transfer" % order.name,
+                'line_ids': line_ids,
+                'journal_id': 3,
+                'date': "2021-4-30",
+                'narration': "Storage contract imported orders transfer",
+            })
+            move_id.post()
+            order.action_release()
         return True
 
 SaleOrder()

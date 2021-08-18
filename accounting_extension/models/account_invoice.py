d67e612e2540606575d2d7aa2619400c2506b97c
aswanth-confianz <aswanth.babu@confianzit.biz>
2021-08-18 13:07:41 +0530

    [ADD] Discount removal strategy.

--- accounting_extension/models/account_invoice.py
+++ accounting_extension/models/account_invoice.py
@@ -2,14 +2,84 @@
 
 from odoo import api, fields, models, _
 from dateutil.relativedelta import relativedelta
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, float_is_zero
 from datetime import datetime
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    def remove_active_discount(self):
+        self.remove_move_reconcile()
+        self.move_id.button_cancel()
+        self.move_id.unlink()
+        return True
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     discount_date = fields.Date('Discount Till')
+
+    @api.model
+    def _get_payments_vals(self):
+        if not self.payment_move_line_ids:
+            return []
+        payment_vals = []
+        currency_id = self.currency_id
+        for payment in self.payment_move_line_ids:
+            payment_currency_id = False
+            if self.type in ('out_invoice', 'in_refund'):
+                amount = sum([p.amount for p in payment.matched_debit_ids if p.debit_move_id in self.move_id.line_ids])
+                amount_currency = sum(
+                    [p.amount_currency for p in payment.matched_debit_ids if p.debit_move_id in self.move_id.line_ids])
+                if payment.matched_debit_ids:
+                    payment_currency_id = all([p.currency_id == payment.matched_debit_ids[0].currency_id for p in
+                                               payment.matched_debit_ids]) and payment.matched_debit_ids[
+                                              0].currency_id or False
+            elif self.type in ('in_invoice', 'out_refund'):
+                amount = sum(
+                    [p.amount for p in payment.matched_credit_ids if p.credit_move_id in self.move_id.line_ids])
+                amount_currency = sum([p.amount_currency for p in payment.matched_credit_ids if
+                                       p.credit_move_id in self.move_id.line_ids])
+                if payment.matched_credit_ids:
+                    payment_currency_id = all([p.currency_id == payment.matched_credit_ids[0].currency_id for p in
+                                               payment.matched_credit_ids]) and payment.matched_credit_ids[
+                                              0].currency_id or False
+            # get the payment value in invoice currency
+            if payment_currency_id and payment_currency_id == self.currency_id:
+                amount_to_show = amount_currency
+            else:
+                currency = payment.company_id.currency_id
+                amount_to_show = currency._convert(amount, self.currency_id, payment.company_id,
+                                                   payment.date or fields.Date.today())
+            if float_is_zero(amount_to_show, precision_rounding=self.currency_id.rounding):
+                continue
+            payment_ref = payment.move_id.name
+            invoice_view_id = None
+            if payment.move_id.ref:
+                payment_ref += ' (' + payment.move_id.ref + ')'
+            if payment.invoice_id:
+                invoice_view_id = payment.invoice_id.get_formview_id()
+            payment_vals.append({
+                'name': payment.name,
+                'journal_name': payment.journal_id.name,
+                'amount': amount_to_show,
+                'currency': currency_id.symbol,
+                'digits': [69, currency_id.decimal_places],
+                'position': currency_id.position,
+                'date': payment.date,
+                'payment_id': payment.id,
+                'account_payment_id': payment.payment_id.id,
+                'payment_name': payment.payment_id.name,
+                'invoice_id': payment.invoice_id.id,
+                'invoice_view_id': invoice_view_id,
+                'move_id': payment.move_id.id,
+                'ref': payment_ref,
+                'is_discount': True if not payment.payment_id and payment.name == 'Discount' else False
+            })
+
+        return payment_vals
+
 
     @api.multi
     def invoice_validate(self):
@@ -109,3 +179,4 @@ class AccountInvoice(models.Model):
                 self.env.cr.execute("update account_move set date=%s where id=%s", (aml.date, move.id))
                 self.env.cr.execute("update account_move_line set date=%s where move_id=%s", (aml.date, move.id))
                 self.env.cr.commit()
+

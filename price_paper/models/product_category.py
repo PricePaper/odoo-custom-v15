871efa3ce5c62f553b069898310d8cc726fb92ec
paul <paul.jose@confianzit.biz>
2021-09-08 09:34:37 +0530

    storage contract boolean in categ. PO UOM domain change

--- price_paper/models/product_category.py
+++ price_paper/models/product_category.py
@@ -6,6 +6,7 @@ from odoo import fields, models, api, _
 class ProductCategory(models.Model):
     _inherit = 'product.category'
 
+    is_storage_contract = fields.Boolean(string='Is storage contract')
     sc_stock_valuation_account_id = fields.Many2one(
         'account.account',
         string="SC Stock Valuation Account"

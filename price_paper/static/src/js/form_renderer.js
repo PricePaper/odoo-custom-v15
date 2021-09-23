1902269b585194f6cd9fa2c63be53e2ffbefa01d
aswanth-confianz <aswanth.babu@confianzit.biz>
2021-09-23 10:26:18 +0530

    [UPDATE] js-patch for sale order confirmation using state(draft, sent)

--- price_paper/static/src/js/form_renderer.js
+++ price_paper/static/src/js/form_renderer.js
@@ -9,7 +9,7 @@ odoo.define('sale_confirm_button.FormRenderer', function (require) {
             var self = this;
             this._super.apply(this, arguments);
             console.log(self);
-            if (self.modelName == "sale.order" && !self.renderer.state.data.id && !self.initialState.data.storage_contract) {
+            if (self.modelName == "sale.order" && ['draft', 'sent'].includes(self.renderer.state.data.state) && !self.initialState.data.storage_contract) {
                 if(confirm("Do you want to Confirm this Sale Order?")) {
                     $("button[name='action_confirm'].btn-secondary").trigger('click');
                 }

1ad5c4988e13aeedfe97adc01b73f8897481963c
aswanth-confianz <aswanth.babu@confianzit.biz>
2021-10-20 16:48:06 +0530

    [BUG-FIX]  vendor check doesn't show discount related to payments. missing relation records added.

--- purchase_extension/wizard/discount_check_generate.py
+++ purchase_extension/wizard/discount_check_generate.py
@@ -25,21 +25,24 @@ class GenerateDiscountCheck(models.TransientModel):
             vendor_bills1 = self.env['account.invoice'].search([('type','=','in_invoice'), ('state', '=', 'open'), ('date_due', '>', self.start_date), ('date_due', '<', self.end_date), ('discount_due_date', '=', False)])
             vendor_bills |= vendor_bills1
             self.wizard_invoice_ids = False
-            lines=[]
+            lines = []
             for bill in vendor_bills:
-                discount_total_amount = bill.amount_total * (100 - bill.payment_term_id.discount_per) /100
-                val={
-                        'total_amount': bill.amount_total,
-                        'partner_id': bill.partner_id.id,
-                        'discount_amount': discount_total_amount,
-                        'invoice_id': bill.id,
-                        'date_invoice': bill.date_invoice,
-                        'date_due': bill.date_due,
-                        'discount_due_date': bill.discount_due_date,
-                        'discount_amount_pseudo': discount_total_amount,
-                        'invoice_id_pseudo': bill.id,
-                        'select': True,
-                    }
+                if bill.discount_due_date and bill.discount_due_date >= fields.Date.today():
+                    discount_total_amount = bill.residual_signed * (100 - bill.payment_term_id.discount_per) / 100
+                else:
+                    discount_total_amount = 0.00
+                val = {
+                    'total_amount': bill.residual_signed,
+                    'partner_id': bill.partner_id.id,
+                    'discount_amount': discount_total_amount,
+                    'invoice_id': bill.id,
+                    'date_invoice': bill.date_invoice,
+                    'date_due': bill.date_due,
+                    'discount_due_date': bill.discount_due_date,
+                    'discount_amount_pseudo': discount_total_amount,
+                    'invoice_id_pseudo': bill.id,
+                    'select': True,
+                }
                 lines.append(val)
 
             vals.update({'wizard_invoice_ids': lines})
@@ -57,15 +60,33 @@ class GenerateDiscountCheck(models.TransientModel):
             raise UserError(_('Bank journal not defined! \nPlease create a bank journal in the system to process these transactions.'))
         if not purchase_writeoff_account:
                 raise UserError(_('Please add a purchase writeoff account in the company form page.'))
-        partner_group={}
+        partner_group = {}
         for invoice in self.wizard_invoice_ids:
+            discount_flag = False
             if not invoice.select:
                 continue
-            if invoice.invoice_id.partner_id.id in partner_group:
-                partner_group[invoice.invoice_id.partner_id.id][0]+=invoice.discount_amount
-                partner_group[invoice.invoice_id.partner_id.id][1].append((4, invoice.invoice_id.id, 0))
+            bill = invoice.invoice_id
+            if bill.discount_due_date and bill.discount_due_date >= fields.Date.today():
+                discount_flag = True
+                payment_line = {
+                    'invoice_id': bill.id,
+                    'discount': invoice.total_amount - invoice.discount_amount,
+                    'discounted_total': invoice.discount_amount,
+                    'amount_total': invoice.total_amount,
+                    'currency_id': bill.currency_id and bill.currency_id.id,
+                    'discount_percentage': bill.payment_term_id.discount_per,
+                    'reference': bill.reference,
+                    'invoice_date': invoice.date_invoice,
+                    'payment_amount': invoice.discount_amount,
+                    'is_full_reconcile': True
+                    }
+            if bill.partner_id.id in partner_group:
+                partner_group[bill.partner_id.id][0] += invoice.discount_amount if discount_flag else invoice.total_amount
+                partner_group[bill.partner_id.id][1].append((4, bill.id, 0))
+                if discount_flag:
+                    partner_group[bill.partner_id.id][2].append((0, 0, payment_line))
             else:
-                partner_group[invoice.invoice_id.partner_id.id] = [invoice.discount_amount, [(4, invoice.invoice_id.id, 0)]]
+                partner_group[bill.partner_id.id] = [invoice.discount_amount, [(4, bill.id, 0)], [(0, 0, payment_line)]] if discount_flag else [invoice.total_amount, [(4, bill.id, 0)], []]
 
         payments = self.env['account.payment']
       
@@ -77,11 +98,15 @@ class GenerateDiscountCheck(models.TransientModel):
                 'partner_id': partner,
                 'amount': partner_group[partner][0],
                 'journal_id': bank_journal.id,
-                'payment_difference_handling': 'reconcile',
-                'writeoff_label': 'Discount',
-                'writeoff_account_id': purchase_writeoff_account.id,
-                'invoice_ids': partner_group[partner][1],
+                'invoice_ids': partner_group[partner][1]
             }
+            if partner_group[partner][2]:
+                payment_vals.update({
+                    'payment_difference_handling': 'reconcile',
+                    'writeoff_label': 'Discount',
+                    'writeoff_account_id': purchase_writeoff_account.id,
+                    'payment_lines': partner_group[partner][2]
+                })
             payments |= payments.create(payment_vals)
         action = self.env.ref('account.action_account_payments_payable').read()[0]
         action['domain'] = [('id', 'in', payments.ids)]

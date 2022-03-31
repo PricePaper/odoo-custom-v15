97ec5bd202b2f4667b866c03c708ff8d4e1a01c0
pauljose456 <paul.jose@confianzit.biz>
2022-03-31 22:56:45 +0530

    STJ price fix

--- purchase_extension/models/purchase_order.py
+++ purchase_extension/models/purchase_order.py
@@ -307,6 +307,26 @@ class PurchaseOrderLine(models.Model):
     gross_volume = fields.Float(string="Gross Volume", compute='_compute_gross_weight_volume')
     gross_weight = fields.Float(string="Gross Weight", compute='_compute_gross_weight_volume')
 
+
+    def _get_stock_move_price_unit(self):
+            
+        self.ensure_one()
+        order = self.order_id
+        price_unit = self.price_unit
+        price_unit_prec = self.env['decimal.precision'].precision_get('Product Price')
+        if self.taxes_id:
+            qty = self.product_qty or 1
+            price_unit = self.taxes_id.with_context(round=False).compute_all(
+                price_unit, currency=self.order_id.currency_id, quantity=qty, product=self.product_id, partner=self.order_id.partner_id
+            )['total_void']
+            price_unit = float_round(price_unit / qty, precision_digits=price_unit_prec)
+        # if self.product_uom.id != self.product_id.uom_id.id:
+        #     price_unit *= self.product_uom.factor / self.product_id.uom_id.factor
+        if order.currency_id != order.company_id.currency_id:
+            price_unit = order.currency_id._convert(
+                price_unit, order.company_id.currency_id, self.company_id, self.date_order or fields.Date.today(), round=False)
+        return price_unit
+
     @api.onchange('product_qty', 'product_uom')
     def _onchange_quantity(self):
         res = super(PurchaseOrderLine, self)._onchange_quantity()

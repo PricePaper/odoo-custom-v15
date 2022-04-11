8e5ba455db59273dbbb613a31768bc62a7766dc4
jeena <jeena@confianzit.biz>
2022-04-11 12:40:48 +0530

    Order Banner

--- batch_delivery/models/order_banner.py
+++ batch_delivery/models/order_banner.py
@@ -6,4 +6,4 @@ class OrderBanner(models.Model):
     _description = 'Banner to be added in processed Sale orders'
 
     code = fields.Char('Order Code',copy=False)
-    name = fields.Char(string='Banner Description')
+    name = fields.Char(string='Banner Description',required=True)

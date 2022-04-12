08e085663cea9c80050cfadba80f789739f5124b
pauljose456 <paul.jose@confianzit.biz>
2022-04-12 11:30:27 +0530

    process returned check

--- account_batch_payment_extension/models/account_move.py
+++ account_batch_payment_extension/models/account_move.py
@@ -4,11 +4,113 @@ from datetime import date
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
+class Accountinvoice(models.Model):
+    _inherit = "account.move"
 
-class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    def remove_bounced_cheque_commission(self):
+        for invoice in self:
+            commission_rec = self.env['sale.commission'].search([
+                ('invoice_id', '=', invoice.id), ('is_paid', '=', True),
+                ('is_cancelled', '=', False), ('invoice_type', '=', 'bounced_cheque')])
+            for rec in commission_rec:
+                if rec.is_settled:
+                    rec.is_cancelled = True
+                    sale = invoice.invoice_line_ids.mapped('sale_line_ids').mapped('order_id')
+                    commission = rec.commission
+                    vals1 = {
+                        'sale_person_id': rec.sale_person_id.id,
+                        'sale_id': sale and sale.id,
+                        'commission': -commission,
+                        'invoice_id': invoice.id,
+                        'invoice_type': 'bounced_reverse',
+                        'is_paid': True,
+                        'invoice_amount': invoice.amount_total,
+                        'commission_date': date.today(),
+                        'paid_date': date.today(),
+                    }
+                    refund_rec = self.env['sale.commission'].create(vals1)
+                else:
+                    rec.unlink()
+        return {}
 
-    # todo not used anywhere
-    # st_date = fields.Date(related='statement_line_id.date', readonly=True, store=True, string='Statement Date')
+    def remove_sale_commission(self, invoice_date):
+
+        invoice_fine = self.env['account.move']
+
+        for invoice in self:
+            commission_rec = self.env['sale.commission'].search([
+                ('invoice_id', '=', invoice.id), ('is_paid', '=', True),
+                ('is_cancelled', '=', False), ('invoice_type', '=', 'out_invoice')])
+            for rec in commission_rec:
+                if rec.is_settled:
+                    rec.is_cancelled = True
+                    sale = invoice.invoice_line_ids.mapped('sale_line_ids')
+                    commission = rec.commission
+                    vals1 = {
+                        'sale_person_id': rec.sale_person_id.id,
+                        'sale_id': sale and sale[-1].order_id.id,
+                        'commission': -commission,
+                        'invoice_id': invoice.id,
+                        'invoice_type': 'bounced_cheque',
+                        'is_paid': True,
+                        'invoice_amount': invoice.amount_total,
+                        'commission_date': date.today(),
+                        'paid_date': date.today(),
+                    }
+                    refund_rec = self.env['sale.commission'].create(vals1)
+                    vals2 = {
+                        'sale_person_id': rec.sale_person_id.id,
+                        'sale_id': sale and sale[-1].order_id.id,
+                        'commission': commission,
+                        'invoice_id': invoice.id,
+                        'invoice_type': 'out_invoice',
+                        'is_paid': False,
+                        'invoice_amount': invoice.amount_total,
+                        'commission_date': invoice.date_invoice and invoice.date_invoice
+                    }
+                    new_rec = self.env['sale.commission'].create(vals2)
+                else:
+                    rec.is_paid = False
+        check_bounce_product = invoice.company_id.check_bounce_product or False
+        if not check_bounce_product:
+            raise UserError(_('Check Bounce Product is not configured in Company'))
+        check_bounce_term = invoice.company_id.check_bounce_term or False
+        if not check_bounce_term:
+            raise UserError(_('Check Bounce Payment Term is not configured in Company'))
+        partner_list = []
+        bounce_invoice = []
+        for invoice in self:
+            if invoice.partner_id.id in partner_list:
+                continue
+            fpos = invoice.fiscal_position_id
+            account = check_bounce_product.product_tmpl_id.get_product_accounts(fpos)
+            if account and account.get('income', ''):
+                account = account['income']
+            line_vals = [(0, 0, {
+                'name': 'Returned check service fee',
+                'account_id': account.id,
+                'product_id': check_bounce_product.id,
+                'price_unit': check_bounce_product.cost,
+                'quantity': 1.0,
+                'discount': 0.0,
+            })]
+            invoice_vals = {
+                'move_type': 'out_invoice',
+                'invoice_date': invoice_date or date.today(),
+                'journal_id': invoice.journal_id.id,
+                'partner_id': invoice.partner_id.id,
+                'partner_shipping_id': invoice.partner_shipping_id.id,
+                'invoice_line_ids': line_vals,
+                'currency_id': invoice.currency_id.id,
+                'invoice_payment_term_id': check_bounce_term.id,
+                'fiscal_position_id': invoice.fiscal_position_id and invoice.fiscal_position_id.id,
+                'team_id': invoice.team_id.id,
+                'check_bounce_invoice': True
+            }
+            invoice_fine = self.env['account.move'].create(invoice_vals)
+            bounce_invoice.append(invoice_fine.id)
+            partner_list.append(invoice.partner_id.id)
+        return bounce_invoice
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- account_batch_payment_extension/models/process_returned_check.py
+++ account_batch_payment_extension/models/process_returned_check.py
@@ -30,18 +30,25 @@ class ProcessReturnedCheck(models.Model):
 
     def process_payment(self):
         for rec in self:
-            payment = rec.payment_ids
-            invoice = payment.mapped('invoice_ids')
             if not rec.payment_ids:
                 raise UserError("Payment is not selected")
+
+            payment = rec.payment_ids
+
             if abs(rec.amount) != sum(payment.mapped('amount')):
                 raise UserError("Amount mismatch.")
-            for pay in rec.payment_ids:
+
+
+            invoice = payment.mapped('reconciled_invoice_ids')
+
+            for pay in payment:
                 pay.write({'old_invoice_ids': [(6, 0, pay.reconciled_invoice_ids.ids)]})
-                pay.mapped('move_line_ids').remove_move_reconcile()
-            reconcile_lines = (payment.mapped('move_line_ids') | self.bank_stmt_line_id.journal_entry_ids)
+                pay.move_id.mapped('line_ids').remove_move_reconcile()
+
+            reconcile_lines = (payment.mapped('line_ids') | self.bank_stmt_line_id.line_ids)
             reconcile_lines = reconcile_lines.filtered(lambda r: not r.reconciled and r.account_id.internal_type in ('payable', 'receivable'))
             reconcile_lines.reconcile()
+
             if invoice:
                 fine_invoices = invoice.remove_sale_commission(self.bank_stmt_line_id.date)
                 if fine_invoices:
@@ -56,11 +63,11 @@ class ProcessReturnedCheck(models.Model):
 
     def action_view_invoice(self):
         invoices = self.mapped('invoice_ids')
-        action = self.sudo().env.ref('account.action_invoice_tree1').read()[0]
+        action = self.sudo().env.ref('account.action_move_out_invoice_type').read()[0]
         if len(invoices) > 1:
             action['domain'] = [('id', 'in', invoices.ids)]
         elif len(invoices) == 1:
-            form_view = [(self.env.ref('account.invoice_form').id, 'form')]
+            form_view = [(self.env.ref('account.view_move_form').id, 'form')]
             if 'views' in action:
                 action['views'] = form_view + [(state, view) for state, view in action['views'] if view != 'form']
             else:

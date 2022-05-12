cc206fdcb093734a85aaa7122fe9f79f28fbb476
jeena <jeena@confianzit.biz>
2022-05-12 13:33:43 +0530

    PP3-T181

--- account_batch_payment_extension/models/account_bank_statement.py
+++ account_batch_payment_extension/models/account_bank_statement.py
@@ -9,6 +9,21 @@ class AccountBankStatementLine(models.Model):
 
     is_return_cleared = fields.Boolean('Return cleared')
 
+
+    def button_undo_reconciliation(self):
+        ''' Undo the reconciliation mades on the statement line and reset their journal items
+        to their original states.
+        '''
+        self.line_ids.remove_move_reconcile()
+        self.payment_ids.unlink()
+
+        for st_line in self:
+            st_line.partner_id = False
+            st_line.with_context(force_delete=True).write({
+                'to_check': False,
+                'line_ids': [(5, 0)] + [(0, 0, line_vals) for line_vals in st_line._prepare_move_line_default_vals()],
+            })
+
     def _seek_for_lines(self):
         ''' Helper used to dispatch the journal items between:
         - The lines using the liquidity account.

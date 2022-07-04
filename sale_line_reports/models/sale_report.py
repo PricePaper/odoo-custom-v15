8546b5d7e167af55a853d6936e5841dcd425d18e
Deviprasad V <deviprasad@confianzit.biz>
2022-07-04 15:42:11 +0530

    sales analysis report enhancement

--- sale_line_reports/models/sale_report.py
+++ sale_line_reports/models/sale_report.py
@@ -11,12 +11,14 @@ class SaleReport(models.Model):
     def _select_sale(self, fields=None):
         if not fields:
             fields = {}
-        fields['sales_persons'] = ", array_to_string(array_agg(distinct c.name),'|') as sales_persons"
+        fields['sales_persons'] = ", array_to_string(array_agg(distinct srp.name),'  |  ') as sp"
         return super(SaleReport, self)._select_sale(fields)
 
     def _from_sale(self, from_clause=''):
         if from_clause:
-            from_clause = "%s join res_partner_sale_order_rel a on a.sale_order_id = s.id join res_partner c on a.res_partner_id = c.id" % from_clause
+            from_clause = """%s join res_partner_sale_order_rel salesperson on salesperson.sale_order_id = s.id 
+                          join res_partner srp on salesperson.res_partner_id = srp.id""" % from_clause
         else:
-            from_clause = " join res_partner_sale_order_rel a on a.sale_order_id = s.id join res_partner c on a.res_partner_id = c.id"
+            from_clause = """ join res_partner_sale_order_rel salesperson on salesperson.sale_order_id = s.id 
+                        join res_partner srp on salesperson.res_partner_id = srp.id"""
         return super(SaleReport, self)._from_sale(from_clause)

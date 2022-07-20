b1a66564ff8696f0a412638ede73275dd5572b12
Deviprasad V <deviprasad@confianzit.biz>
2022-07-20 19:38:55 +0530

    last and current fiscal year feature added

--- sale_line_reports/models/sale_report.py
+++ sale_line_reports/models/sale_report.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+from datetime import date, datetime
 
 
 class SaleReport(models.Model):
@@ -22,3 +24,61 @@ class SaleReport(models.Model):
             from_clause = """ join res_partner_sale_order_rel salesperson on salesperson.sale_order_id = s.id
                         join res_partner srp on salesperson.res_partner_id = srp.id"""
         return super(SaleReport, self)._from_sale(from_clause)
+
+    def _get_fiscal_year(self, date, mode='current'):
+        if mode == 'last':
+            if int(self.env.user.company_id.fiscalyear_last_month) > date.month:
+                start_date = datetime.strptime("%s-%s-%s" % (date.year - 2, int(self.env.user.company_id.fiscalyear_last_month) + 1, 1),
+                                               DEFAULT_SERVER_DATE_FORMAT)
+                end_date = datetime.strptime(
+                    "%s-%s-%s" % (date.year - 1, int(self.env.user.company_id.fiscalyear_last_month), self.env.user.company_id.fiscalyear_last_day),
+                    DEFAULT_SERVER_DATE_FORMAT)
+            else:
+                start_date = datetime.strptime("%s-%s-%s" % (date.year - 1, int(self.env.user.company_id.fiscalyear_last_month) + 1, 1),
+                                               DEFAULT_SERVER_DATE_FORMAT)
+                end_date = datetime.strptime(
+                    "%s-%s-%s" % (date.year, self.env.user.company_id.fiscalyear_last_month, self.env.user.company_id.fiscalyear_last_day),
+                    DEFAULT_SERVER_DATE_FORMAT)
+        else:
+            if int(self.env.user.company_id.fiscalyear_last_month) > date.month:
+                start_date = datetime.strptime("%s-%s-%s" % (date.year - 1, int(self.env.user.company_id.fiscalyear_last_month) + 1, 1),
+                                               DEFAULT_SERVER_DATE_FORMAT)
+                end_date = datetime.strptime(
+                    "%s-%s-%s" % (date.year, int(self.env.user.company_id.fiscalyear_last_month), self.env.user.company_id.fiscalyear_last_day),
+                    DEFAULT_SERVER_DATE_FORMAT)
+            else:
+                start_date = datetime.strptime("%s-%s-%s" % (date.year, int(self.env.user.company_id.fiscalyear_last_month) + 1, 1),
+                                               DEFAULT_SERVER_DATE_FORMAT)
+                end_date = datetime.strptime(
+                    "%s-%s-%s" % (date.year + 1, self.env.user.company_id.fiscalyear_last_month, self.env.user.company_id.fiscalyear_last_day),
+                    DEFAULT_SERVER_DATE_FORMAT)
+        return start_date, end_date
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        new_dom = []
+        for dom in domain:
+            if len(dom) == 3 and dom[0] == 'name' and dom[2] == 'current_fiscal_year':
+                start_date, end_date = self._get_fiscal_year(date.today())
+                new_dom = ['&', ['date', '>=', start_date], ['date', '<', end_date]]
+                break
+            elif len(dom) == 3 and dom[0] == 'name' and dom[2] == 'last_fiscal_year':
+                start_date, end_date = self._get_fiscal_year(date.today(), 'last')
+                new_dom = ['&', ['date', '>=', start_date], ['date', '<', end_date]]
+                break
+        if new_dom:
+            index = domain.index(dom)
+            if index != 0 and len(domain[index - 1]) == 1:
+                domain.pop(index - 1)
+            elif index != 0:
+                reverse_index = 0
+                for i in range(index, 0, -1):
+                    if len(domain[i]) != 1:
+                        reverse_index += 1
+                    else:
+                        break
+                if index - reverse_index >= 0:
+                    domain.pop(index - reverse_index)
+            domain.remove(dom)
+            domain = new_dom + domain
+        return super().read_group(domain, fields, groupby, offset, limit, orderby, lazy)

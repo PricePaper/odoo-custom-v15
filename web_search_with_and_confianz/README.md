cb5449f0b0ee5c7ac4c46b6847e5f8c365b436c6
Deviprasad V <deviprasad@confianzit.biz>
2022-08-26 21:34:58 +0530

    module added with new name

---
+++ web_search_with_and_confianz/README.md
@@ -0,0 +1,18 @@
+When searching for records on same field Odoo joins multiple queries with OR.
+For example:
+
+* Perform a search for customer "John" on field Name
+* Odoo displays customers containing "John"
+* Search for "Smith" on same field Name
+* Odoo displays customers containing "John" OR "Smith"
+
+With this module installed you can press Shift key before searching for "Smith"
+and Odoo finds customers containing "John" AND "Smith"
+
+
+#Usage:
+
+* Enter your value in omnibar search field
+* Press and hold Shift key
+* Select field with mouse or keyboard and press Enter key to perform search
+
---
+++ web_search_with_and_confianz/static/src/js/components/main.js
@@ -0,0 +1,79 @@
+/** @odoo-module **/
+
+import {patch} from 'web.utils';
+
+const Domain = require('web.Domain');
+const pyUtils = require('web.py_utils');
+const SearchBar = require("web.SearchBar");
+const ActionModel = require("web.ActionModel");
+const ControlPanelModelExtension = require("web/static/src/js/control_panel/control_panel_model_extension.js");
+
+
+patch(SearchBar.prototype, 'web_search_with_and', {
+    _onSearchKeydown(ev) {
+        if (ev.shiftKey && ev.key == "Enter") {
+            this.model.config.context.webSearchWithOR = true;
+        } else {
+            this.model.config.context.webSearchWithOR = false;
+        }
+        this._super(...arguments);
+    },
+    _onWindowClick(ev) {
+        if (ev.shiftKey && ev.pointerType == "mouse" && ev.type == "click") {
+            this.model.config.context.webSearchWithOR = true;
+        } else {
+            this.model.config.context.webSearchWithOR = false;
+        }
+
+        this._super(...arguments);
+    }
+});
+
+
+patch(ControlPanelModelExtension.prototype, 'web_search_with_and', {
+    _getAutoCompletionFilterDomain(filter, filterQueryElements) {
+        // return this._super(...arguments);
+
+        const domains = filterQueryElements.map(({
+            label,
+            value,
+            operator
+        }) => {
+            let domain;
+            if (filter.filterDomain) {
+                domain = Domain.prototype.stringToArray(
+                    filter.filterDomain, {
+                        self: label,
+                        raw_value: value,
+                    }
+                );
+            } else {
+                domain = [[filter.fieldName, operator, value]];
+            }
+            return Domain.prototype.arrayToString(domain);
+        });
+
+        if (this.config.context.webSearchWithOR) {
+            return pyUtils.assembleDomains(domains, 'OR');
+        } else {
+            return pyUtils.assembleDomains(domains, 'AND');
+        }
+    }
+});
+
+
+patch(ActionModel.prototype, 'web_search_with_and', {
+    _getFacets() {
+        let self = this;
+        let facets = this._super(...arguments);
+
+        if (!self.config.context.webSearchWithOR) {
+            _(facets).each(function (facet) {
+                if (facet.type == 'field' && facet.separator == self.env._t("or")) {
+                    facet.separator = self.env._t("and")
+                }
+            })
+        }
+        return facets;
+    }
+});

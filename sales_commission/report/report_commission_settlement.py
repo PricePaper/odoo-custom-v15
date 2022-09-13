e5e70f1fe669387c6b5c478f356267c88a344464
pauljose456 <paul.jose@confianzit.biz>
2022-09-13 15:41:13 +0530

    commission settlement view and report changes

--- sales_commission/report/report_commission_settlement.py
+++ sales_commission/report/report_commission_settlement.py
@@ -14,23 +14,6 @@ class Reportcommission_audit(models.AbstractModel):
     _name = "report.sales_commission.report_commission_settlement"
     _description = 'Commission Settlement report'
 
-    def get_commission_lines(self, doc):
-        commission_vals={}
-        for commission in doc.commission_ids:
-            if not commission.invoice_id:
-                if commission_vals.get(False):
-                    commission_vals[False].append(commission)
-                else:
-                    commission_vals[False] = [commission]
-                continue
-
-            if commission_vals.get(commission.invoice_id.partner_id):
-                commission_vals[commission.invoice_id.partner_id].append(commission)
-            else:
-                commission_vals[commission.invoice_id.partner_id] = [commission]
-        return commission_vals
-
-
     @api.model
     def _get_report_values(self, docids, data=None):
         docs = self.env['sale.commission.settlement'].browse(docids)
@@ -38,7 +21,6 @@ class Reportcommission_audit(models.AbstractModel):
                 'doc_model': 'sale.commission.settlement',
                 'docs': docs,
                 'data': data,
-                'get_commission_lines': self.get_commission_lines,
             }
 
 

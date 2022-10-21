1827f47bbaa80af82c457253acf9098c238620d4
pauljose456 <paul.jose@confianzit.biz>
2022-10-21 17:11:04 +0530

    PO/RFQ pdf report chnages

--- purchase_extension/models/res_company.py
+++ purchase_extension/models/res_company.py
@@ -9,7 +9,7 @@ class ResCompany(models.Model):
     @api.model
     def _get_default_purchase_msg(self):
         result = """
-            <div style = "font-family: 'Gotham Bold', sans-serif;font-size:14px;" >
+            <div style = "font-family: 'Gotham Bold', sans-serif;font-size:7px;" >
                 <p>Purchase Orders must be confirmed via e-mail or phone. Delivery appointments are required. Receiving hours are from M-F between 6 AM and 2 PM, excluding holidays.
                 <span>POs are valid up to 30 days from the wanted date. Shipments must be unloaded to dock and are pallet exchange, only.</span>
                 Print over-runs of more than 10% will not be accepted. Thank you.</p>

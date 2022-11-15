fae50a975f9aebb622f5ad230aa94bc573c62190
pauljose456 <paul.jose@confianzit.biz>
2022-11-15 12:31:39 +0530

    rma invoice after picking done

--- rma_extension/models/rma.py
+++ rma_extension/models/rma.py
@@ -336,7 +336,7 @@ class RMARetMerAuth(models.Model):
                 state = 'resolved'
                 exchange_move_vals = []
                 stock_moves_vals = []
-                invoice_line_vals = []
+                # invoice_line_vals = []
                 exchange_inv_line_vals = []
                 for rma_line in rma.rma_sale_lines_ids:
                     state = 'approve'
@@ -360,42 +360,42 @@ class RMARetMerAuth(models.Model):
                         'to_refund': True
                     }
                     stock_moves_vals.append((0, 0, rma_move_vals_b2b))
-                    inv_account_id = rma_line.product_id. \
-                                         property_account_income_id and \
-                                     rma_line.product_id. \
-                                         property_account_income_id.id or \
-                                     rma_line.product_id.categ_id. \
-                                         property_account_income_categ_id and \
-                                     rma_line.product_id.categ_id. \
-                                         property_account_income_categ_id.id or False
-                    if not inv_account_id:
-                        raise ValidationError((
-                                                  'No account defined for product "%s".') %
-                                              rma_line.product_id.name)
-                    prod_price = 0.0
-                    if rma_line.refund_qty != 0:
-                        prod_price = float(
-                            (rma_line.refund_price) / float(
-                                rma_line.refund_qty))
-                    inv_line_values = {
-                        'product_id': rma_line.product_id and rma_line.
-                            product_id.id or False,
-                        'account_id': inv_account_id or False,
-                        'name': rma_line.product_id and rma_line.
-                            product_id.name or False,
-                        'quantity': rma_line.refund_qty or 0,
-                        'product_uom_id': rma_line.return_product_uom and rma_line.return_product_uom.id or False,
-                        'price_unit': prod_price or 0,
-                        'currency_id': rma.currency_id.id or False,
-                        'sale_line_ids': [(6, 0, [rma_line.so_line_id.id])]
-                    }
-
-                    if rma_line.tax_id and rma_line.tax_id.ids:
-                        inv_line_values.update(
-                            {'tax_ids': [(6, 0, rma_line.
-                                                       tax_id.ids)]})
-
-                    invoice_line_vals.append((0, 0, inv_line_values))
+                    # inv_account_id = rma_line.product_id. \
+                    #                      property_account_income_id and \
+                    #                  rma_line.product_id. \
+                    #                      property_account_income_id.id or \
+                    #                  rma_line.product_id.categ_id. \
+                    #                      property_account_income_categ_id and \
+                    #                  rma_line.product_id.categ_id. \
+                    #                      property_account_income_categ_id.id or False
+                    # if not inv_account_id:
+                    #     raise ValidationError((
+                    #                               'No account defined for product "%s".') %
+                    #                           rma_line.product_id.name)
+                    # prod_price = 0.0
+                    # if rma_line.refund_qty != 0:
+                    #     prod_price = float(
+                    #         (rma_line.refund_price) / float(
+                    #             rma_line.refund_qty))
+                    # inv_line_values = {
+                    #     'product_id': rma_line.product_id and rma_line.
+                    #         product_id.id or False,
+                    #     'account_id': inv_account_id or False,
+                    #     'name': rma_line.product_id and rma_line.
+                    #         product_id.name or False,
+                    #     'quantity': rma_line.refund_qty or 0,
+                    #     'product_uom_id': rma_line.return_product_uom and rma_line.return_product_uom.id or False,
+                    #     'price_unit': prod_price or 0,
+                    #     'currency_id': rma.currency_id.id or False,
+                    #     'sale_line_ids': [(6, 0, [rma_line.so_line_id.id])]
+                    # }
+                    #
+                    # if rma_line.tax_id and rma_line.tax_id.ids:
+                    #     inv_line_values.update(
+                    #         {'tax_ids': [(6, 0, rma_line.
+                    #                                    tax_id.ids)]})
+                    #
+                    # invoice_line_vals.append((0, 0, inv_line_values))
 
                     if rma_line.type == 'exchange':
                         state = 'approve'
@@ -519,40 +519,40 @@ class RMARetMerAuth(models.Model):
                     if PK_EXC.mapped('move_lines').filtered(lambda move: move.state not in ('draft', 'cancel', 'done')):
                         PK_EXC.action_assign()
 
-                if invoice_line_vals:
-                    inv_values = {
-                        'move_type': 'out_refund',
-                        'invoice_origin': rma.name or '',
-                        #'comment': rma.problem or '',
-                        'partner_id': rma.invoice_address_id and
-                                      rma.invoice_address_id.id or False,
-                        #'account_id':
-                          #  rma.invoice_address_id.property_account_receivable_id and
-                           # rma.invoice_address_id.property_account_receivable_id.id or
-                           # False,
-                        'invoice_line_ids': invoice_line_vals,
-                        'invoice_date': rma.rma_date or False,
-                        'rma_id': rma.id,
-                    }
-                    salereps = rma.mapped('rma_sale_lines_ids').mapped('so_line_id').mapped('order_id').mapped('sales_person_ids')
-                    commission_rule_ids = rma.mapped('rma_sale_lines_ids').mapped('so_line_id').mapped('order_id').mapped('commission_rule_ids')
-                    if salereps:
-                        inv_values['sales_person_ids'] = [(6, 0, salereps.ids)]
-                    if commission_rule_ids:
-                        inv_values['commission_rule_ids'] = [(6, 0, commission_rule_ids.ids)]
-                    inv = self.env['account.move'].with_context(mail_create_nosubscribe=True).create(inv_values)
-                    for inv_line in inv.invoice_line_ids:
-                        for move_line in PK_IN.move_lines:
-                            if inv_line.product_id == move_line.product_id and inv_line.quantity == move_line.product_uom_qty:
-                                inv_line.write({'stock_move_id': move_line.id})
-                                break
+                # if invoice_line_vals:
+                #     inv_values = {
+                #         'move_type': 'out_refund',
+                #         'invoice_origin': rma.name or '',
+                #         #'comment': rma.problem or '',
+                #         'partner_id': rma.invoice_address_id and
+                #                       rma.invoice_address_id.id or False,
+                #         #'account_id':
+                #           #  rma.invoice_address_id.property_account_receivable_id and
+                #            # rma.invoice_address_id.property_account_receivable_id.id or
+                #            # False,
+                #         'invoice_line_ids': invoice_line_vals,
+                #         'invoice_date': rma.rma_date or False,
+                #         'rma_id': rma.id,
+                #     }
+                #     salereps = rma.mapped('rma_sale_lines_ids').mapped('so_line_id').mapped('order_id').mapped('sales_person_ids')
+                #     commission_rule_ids = rma.mapped('rma_sale_lines_ids').mapped('so_line_id').mapped('order_id').mapped('commission_rule_ids')
+                #     if salereps:
+                #         inv_values['sales_person_ids'] = [(6, 0, salereps.ids)]
+                #     if commission_rule_ids:
+                #         inv_values['commission_rule_ids'] = [(6, 0, commission_rule_ids.ids)]
+                #     inv = self.env['account.move'].with_context(mail_create_nosubscribe=True).create(inv_values)
+                #     for inv_line in inv.invoice_line_ids:
+                #         for move_line in PK_IN.move_lines:
+                #             if inv_line.product_id == move_line.product_id and inv_line.quantity == move_line.product_uom_qty:
+                #                 inv_line.write({'stock_move_id': move_line.id})
+                #                 break
 
                 if exchange_inv_line_vals:
                     ex_inv_vals = {
                         'move_type': 'out_invoice',
                         #'comment': rma.problem or '',
                         'invoice_origin': rma.name or '',
-                      
+
                         'partner_id': rma.invoice_address_id and
                                       rma.invoice_address_id.id or False,
                         # 'account_id':

f0db493652cda43e94ccf31fc1cc5a0e5aa55c09
pauljose456 <paul.jose@confianzit.biz>
2023-03-25 19:07:21 +0530

    UPS initial commit

---
+++ UPS_delivery_ext/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'UPS Extension',
+    'version': '15.0.0',
+    'license': 'LGPL-3',
+    'summary': 'UPS Extension',
+    'author': 'Confianz',
+    'website': 'https://www.confianzit.com',
+    'description': """
+    UPS integration extension for PPT
+    """,
+    'data': [],
+    'depends': [
+        'delivery_ups'
+    ],
+    'installable': True,
+    'application': False,
+}

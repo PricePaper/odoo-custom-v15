c0bd2823c85c9a714bd39be3feb26a97564ac43e
pauljose456 <paul.jose@confianzit.biz>
2023-05-09 14:20:53 +0530

    SC margin FIX

--- price_paper/models/sale.py
+++ price_paper/models/sale.py
@@ -1101,18 +1101,22 @@ class SaleOrderLine(models.Model):
     def _compute_lst_cost_prices(self):
         for line in self:
             if line.product_id and line.product_uom:
-                uom_price = line.product_id.uom_standard_prices.filtered(lambda r: r.uom_id == line.product_uom)
-                if uom_price:
-                    line.lst_price = uom_price[0].price
-                    if line.product_id.cost:
-                        line.working_cost = uom_price[0].cost
+                if line.storage_contract_line_id:
+                    line.working_cost = 0
+                    line.lst_price = 0
                 else:
-                    line.product_id.job_queue_standard_price_update()
                     uom_price = line.product_id.uom_standard_prices.filtered(lambda r: r.uom_id == line.product_uom)
                     if uom_price:
                         line.lst_price = uom_price[0].price
                         if line.product_id.cost:
                             line.working_cost = uom_price[0].cost
+                    else:
+                        line.product_id.job_queue_standard_price_update()
+                        uom_price = line.product_id.uom_standard_prices.filtered(lambda r: r.uom_id == line.product_uom)
+                        if uom_price:
+                            line.lst_price = uom_price[0].price
+                            if line.product_id.cost:
+                                line.working_cost = uom_price[0].cost
             if line.is_delivery and line.order_id.carrier_id and line.order_id.carrier_id.delivery_type not in [
                 'base_on_rule', 'fixed']:
                 line.working_cost = line.order_id.delivery_cost

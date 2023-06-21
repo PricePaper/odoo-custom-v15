d97c83f30507fea80a27eac788ab5d9d810f596c
pauljose456 <paul.jose@confianzit.biz>
2023-06-21 13:55:37 +0530

    cost change report

--- price_maintanance/models/__init__.py
+++ price_maintanance/models/__init__.py
@@ -6,5 +6,6 @@ from . import price_edit_notes
 from . import cost_change
 from . import res_partner
 from . import res_company
+from . import res_config_settings
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

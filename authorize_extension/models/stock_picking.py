1203c1e00043484b3e85ff0784b483b965b2844c
pauljose456 <paul.jose@confianzit.biz>
2023-08-04 14:02:41 +0530

    changes in authorize pending

--- authorize_extension/models/stock_picking.py
+++ authorize_extension/models/stock_picking.py
@@ -22,7 +22,7 @@ class StockMove(models.Model):
                 order = self.picking_id.sale_id
                 if order:
                     if order.is_transaction_pending or order.is_transaction_error:
-                        raise UserError('You can not change the state of %s. Payment Transaction is in hold or error state please contact Accountant'%(self.picking_id.name))
+                        raise UserError('%s is on hold or in error state, and the Truck Batch is confirmed. Please contact Sales'%(self.picking_id.name))
                     if order.credit_hold_after_confirm:
                         raise UserError('You can not change the state of %s. Sale order is in credit hold state please contact Credit manager'%(self.picking_id.name))
         return super().write(vals)

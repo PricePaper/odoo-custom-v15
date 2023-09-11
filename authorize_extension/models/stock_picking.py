4dccc9f1e5fd028cd2f3fbf53c15e52a68a0f064
pauljose456 <paul.jose@confianzit.biz>
2023-09-11 09:31:24 +0530

    FIX

--- authorize_extension/models/stock_picking.py
+++ authorize_extension/models/stock_picking.py
@@ -18,11 +18,12 @@ class StockMove(models.Model):
 
     def write(self, vals):
         if vals.get('state', False):
-            if self.picking_id.is_payment_hold:
-                order = self.picking_id.sale_id
-                if order:
-                    if order.is_transaction_pending or order.is_transaction_error:
-                        raise UserError('%s is on hold or in error state, and the Truck Batch is confirmed. Please contact Sales'%(self.picking_id.name))
-                    if order.credit_hold_after_confirm:
-                        raise UserError('You can not change the state of %s. Sale order is in credit hold state please contact Credit manager'%(self.picking_id.name))
+            for move in self:
+                if move.picking_id.is_payment_hold:
+                    order = move.picking_id.sale_id
+                    if order:
+                        if order.is_transaction_pending or order.is_transaction_error:
+                            raise UserError('%s is on hold or in error state, and the Truck Batch is confirmed. Please contact Sales'%(self.picking_id.name))
+                        if order.credit_hold_after_confirm:
+                            raise UserError('You can not change the state of %s. Sale order is in credit hold state please contact Credit manager'%(self.picking_id.name))
         return super().write(vals)

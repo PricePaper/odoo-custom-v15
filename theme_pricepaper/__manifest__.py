4a6e22e011c7ac0e40bf2ad7a2f25728b02ab52d
Shivam Gupta <shivam.gupta@confianzit.com>
2023-11-06 01:02:07 +0530

    [add]theme_pricepaper

---
+++ theme_pricepaper/__manifest__.py
@@ -0,0 +1,43 @@
+{
+    'name': 'Theme Pricepaper',
+    'version': '1.0',
+    'description': 'Custom theme for pricepaper',
+    'summary': 'Theme especially designed for the pricepaper, for managing the ecom',
+    'author': 'Confianz Global Pvt. Ltd.',
+    'website': 'https://www.confianzit.com/',
+    'license': 'LGPL-3',
+    'category': 'Theme/eCommerce',
+    'depends': [
+        'website_sale'
+    ],
+    'data': [
+        'views/header/header.xml',
+        'views/footer/footer.xml',
+        'views/snippet_options.xml',
+        'views/snippets/banner.xml',
+        'views/snippets/video_banner.xml',
+        'views/snippets/good_fit.xml',
+        'views/snippets/your_product.xml',
+        'views/snippets/price_paper.xml',
+        'views/snippets/manufactures.xml',
+        'views/snippets/contact_us.xml',
+        'views/snippets.xml',
+    ],
+    'demo': [
+        ''
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'theme_pricepaper/static/src/js/main.js',
+            'theme_pricepaper/static/src/scss/header.scss',
+            'theme_pricepaper/static/src/scss/footer.scss',
+            'theme_pricepaper/static/src/scss/common.scss',
+            'theme_pricepaper/static/src/scss/banner.scss',
+            'theme_pricepaper/static/src/scss/snippet/*',
+        ],
+    },
+    'auto_install': False,
+    'application': False,
+   
+}
+

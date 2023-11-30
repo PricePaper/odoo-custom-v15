75d0c19571061de5f7a2649f9b2c2597e9ac57c0
Shivam Gupta <shivam.gupta@confianzit.com>
2023-11-30 11:47:29 +0530

    [ADD]Fonts

--- theme_pricepaper/__manifest__.py
+++ theme_pricepaper/__manifest__.py
@@ -29,9 +29,10 @@
     'demo': [
         ''
     ],
+    
     'assets': {
         'web.assets_frontend': [
-            "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap",
+            # "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
             'theme_pricepaper/static/src/scss/common.scss',
             'theme_pricepaper/static/src/lib/owl_carosuell/*',
             'theme_pricepaper/static/src/scss/header.scss',

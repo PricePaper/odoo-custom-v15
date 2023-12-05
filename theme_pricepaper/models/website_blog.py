0bbd33c7c84ce7b134e5142d639c67ec35721ce5
Shivam Gupta <shivam.gupta@confianzit.com>
2023-12-05 12:28:53 +0530

    [upd]

--- theme_pricepaper/models/website_blog.py
+++ theme_pricepaper/models/website_blog.py
@@ -1,15 +1,18 @@
 from odoo import fields,api,models
 from odoo.addons.website.tools import text_from_html
+from odoo.tools.translate import html_translate
 import re
 class BlogPost(models.Model):
     _inherit = 'blog.post'
 
     def _default_content(self):
-        import pdb 
-        pdb.set_trace()
+        # res = super(BlogPost,self)._default_content()
+        print('hellooo')
         return '''
             
         '''
+    content = fields.Html('Content', default=_default_content, translate=html_translate, sanitize=False)
+
     @api.depends('content', 'teaser_manual')
     def _compute_teaser(self):
         for blog_post in self:

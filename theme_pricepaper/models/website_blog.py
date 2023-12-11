112b3a59142816eeebf5b8543afcee31b9c42288
Shivam Gupta <shivam.gupta@confianzit.com>
2023-12-11 17:01:59 +0530

    [upd]

--- theme_pricepaper/models/website_blog.py
+++ theme_pricepaper/models/website_blog.py
@@ -11,8 +11,29 @@ class BlogPost(models.Model):
         return '''
             
         '''
-    content = fields.Html('Content', default=_default_content, translate=html_translate, sanitize=False)
+    def _default_content_summary(self):
+        return'''
+        Some of our customers are transitioning to paper bags regardless due to public opinion. This is a business choice; they are concerned that their customers will believe they are breaking the law or they are choosing what is perceived to be more environmentally friendly solution . However, it is not mandatory if your business falls into one of the above categories.
+        '''
+    def _default_content_summary1(self):
+        return'''
+        A large portion of our customers are exempt from the ban and may continue to use plastic bags if they so choose.
+        '''
 
+    def _default_content_subtitle2(self):
+        return'''
+        Executive Summary :
+        '''
+
+    def _default_content_test(self):
+        return'''
+          <img class="img-fluid" width="642" height="773" src="/theme_pricepaper/static/src/img/paper-bags.webp" alt=""/>
+        '''
+    content = fields.Html('Content', default=_default_content, translate=html_translate, sanitize=False)
+    summary = fields.Html('Summary', default=_default_content_summary, translate=html_translate, sanitize=False)
+    summary1 = fields.Html('Summary', default=_default_content_summary1, translate=html_translate, sanitize=False)
+    subtitle2 = fields.Html('subtitle2', default=_default_content_subtitle2, translate=html_translate, sanitize=False)
+    img_cover = fields.Html('subtitle2', default=_default_content_test, translate=html_translate, sanitize=False)
     @api.depends('content', 'teaser_manual')
     def _compute_teaser(self):
         for blog_post in self:

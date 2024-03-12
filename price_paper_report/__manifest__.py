577d8854b8c7f5006f549afa2360ccdb5980e582
pauljose456 <paul.jose@confianzit.biz>
2024-03-12 11:53:04 +0530

    price_paper new report

---
+++ price_paper_report/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Confianz IT
+#    Copyright (C) 2021   (https://www.confianzit.com)
+#
+##############################################################################
+
+{
+    'name': 'Price Paper Report',
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'category': 'Sales and Purchase',
+    'summary': "Reports for Pricepaper",
+    'description': """
+Custom module implemented for Price Papers Reports.
+=================================================================
+Custom module implemented for Price Paper Reports.
+       """,
+    'author': 'Confianz Global',
+    'website': 'http://confianzit.com',
+    'images': [],
+
+    'data': [
+        'views/sale_report.xml',
+    ],
+
+
+    'depends': ['instant_invoice'],
+
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

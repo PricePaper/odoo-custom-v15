ec97dc2f0ad996c363173a9c098923b6edc53433
harikrishnan <harikrishnan@confianzit.com>
2024-05-21 12:33:25 +0530

    add payment_acquirer'

--- ppt_mobile_apis/models/partner.py
+++ ppt_mobile_apis/models/partner.py
@@ -22,27 +22,27 @@ class Partner(models.Model):
             if default_payment_term:
                 payment_terms = {
                     'payment_term': default_payment_term.name,
-                    'payment_term_id':default_payment_term.id,
+                    'payment_term_id': default_payment_term.id,
                     'payment_methods': []
                 }
 
                 if default_payment_term.payment_method == 'ach-debit':
                     payment_terms['payment_methods'] = [
-                        {'name': 'Credit Card', 'availability': False, 'default': False},
-                        {'name': 'COD', 'availability': False, 'default': False},
-                        {'name': 'ACH-Debit', 'availability': True, 'default': True}
+                        {'name': 'Credit Card', 'availability': False, 'payment_acquirer': 'authorize', 'default': False},
+                        {'name': 'COD', 'availability': False, 'payment_acquirer': 'cod', 'default': False},
+                        {'name': 'ACH-Debit', 'availability': True, 'payment_acquirer': 'ach-debit', 'default': True}
                     ]
                 elif default_payment_term.is_pre_payment:
                     payment_terms['payment_methods'] = [
-                        {'name': 'Credit Card', 'availability': True, 'default': True},
-                        {'name': 'COD', 'availability': False, 'default': False},
-                        {'name': 'ACH-Debit', 'availability': True, 'default': False}
+                        {'name': 'Credit Card', 'availability': True, 'payment_acquirer': 'authorize', 'default': True},
+                        {'name': 'COD', 'availability': False, 'payment_acquirer': 'cod', 'default': False},
+                        {'name': 'ACH-Debit', 'availability': True, 'payment_acquirer': 'ach-debit', 'default': False}
                     ]
                 elif default_payment_term.is_discount:
                     payment_terms['payment_methods'] = [
-                        {'name': 'Credit Card', 'availability': True, 'default': False},
-                        {'name': 'COD', 'availability': True, 'default': True},
-                        {'name': 'ACH-Debit', 'availability': False, 'default': False}
+                        {'name': 'Credit Card', 'availability': True, 'payment_acquirer': 'authorize', 'default': False},
+                        {'name': 'COD', 'availability': True, 'payment_acquirer': 'cod','default': True},
+                        {'name': 'ACH-Debit', 'availability': False, 'payment_acquirer': 'ach-debit', 'default': False}
                     ]
 
                 result.append(payment_terms)

9aaeeac01611e3f12a9694c19d7019109356b6c8
harikrishnan <harikrishnan@confianzit.com>
2024-05-31 09:32:34 +0530

    [update] sale order confirm

--- ppt_mobile_apis/models/sale_order.py
+++ ppt_mobile_apis/models/sale_order.py
@@ -12,15 +12,10 @@ class SaleOrder(models.Model):
 
         message = {'success': False,
                    'error': False}
-        if not self.token_id:
-            message['error'] = 'Payment Token Not Found'
-            result.append(message)
-            return result
 
         res = self.action_confirm()
 
         if isinstance(res, dict):
-            message['success'] = False
             message['error'] = res.get('context', {}).get('default_warning_message', 'No warning message provided')
         elif res:
             message['success'] = True

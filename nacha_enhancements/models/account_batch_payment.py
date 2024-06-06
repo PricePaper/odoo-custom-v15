fd8b60bb7879ae6d4bee6bb1c37683dc62a0f967
pauljose456 <paul.jose@confianzit.biz>
2024-06-06 18:57:37 +0530

    ACH customer

--- nacha_enhancements/models/account_batch_payment.py
+++ nacha_enhancements/models/account_batch_payment.py
@@ -50,14 +50,16 @@ class AccountBatchPayment(models.Model):
     def _generate_nacha_batch_header_record(self, payment, batch_nr):
         batch = []
         batch.append("5")  # Record Type Code
-        batch.append("220")  # Service Class Code (credits only)
-
+        if self.batch_type == 'outbound':
+            batch.append("220")  # Service Class Code (credits only)
+        if self.batch_type == 'inbound':
+            batch.append("225")  # Service Class Code (Debits only)
         formatted_company_name = ''.join(char.upper() if char.isalpha() else ' ' for char in self.journal_id.company_id.name)
         batch.append("{:16.16}".format(formatted_company_name))  # Company Name
 
         batch.append("{:0>20.20}".format(self.journal_id.nacha_company_chase_account))  # Company Discretionary Data
         batch.append("{:0>10.10}".format(self.journal_id.nacha_company_identification))  # Company Identification
-        batch.append("PPD")  # Standard Entry Class Code
+        batch.append("CCD")  # Standard Entry Class Code
 
         formatted_reference = ''.join(char.upper() if char.isalpha() else char if char.isnumeric() else '' for char in payment.ref)
         batch.append("{:10.10}".format(formatted_reference))  # Company Entry Description
@@ -75,7 +77,10 @@ class AccountBatchPayment(models.Model):
         bank = payment.partner_bank_id
         entry = []
         entry.append("6")  # Record Type Code (PPD)
-        entry.append("22")  # Transaction Code
+        if self.batch_type == 'outbound':
+            entry.append("22")  # Transaction Code (credits only)
+        if self.batch_type == 'inbound':
+            entry.append("27")  # Transaction Code (Debits only)
         entry.append("{:8.8}".format(bank.aba_routing[:-1]))  # RDFI Routing Transit Number
         entry.append("{:1.1}".format(bank.aba_routing[-1]))  # Check Digit
         entry.append("{:17.17}".format(bank.acc_number))  # DFI Account Number
@@ -95,3 +100,23 @@ class AccountBatchPayment(models.Model):
         entry.append("{:07d}".format(0))  # Trace Number (88-94)
 
         return "".join(entry)
+
+    def _generate_nacha_batch_control_record(self, payment, batch_nr):
+        bank = payment.partner_bank_id
+        control = []
+        control.append("8")  # Record Type Code
+        if self.batch_type == 'outbound':
+            control.append("220")  # Service Class Code (credits only)
+        if self.batch_type == 'inbound':
+            control.append("225")  # Service Class Code (Debits only)
+        control.append("{:06d}".format(1))  # Entry/Addenda Count
+        control.append("{:010d}".format(self._calculate_aba_hash(bank.aba_routing)))  # Entry Hash
+        control.append("{:012d}".format(0))  # Total Debit Entry Dollar Amount in Batch
+        control.append("{:012d}".format(round(payment.amount * 100)))  # Total Credit Entry Dollar Amount in Batch
+        control.append("{:0>10.10}".format(self.journal_id.nacha_company_identification))  # Company Identification
+        control.append("{:19.19}".format(""))  # Message Authentication Code (leave blank)
+        control.append("{:6.6}".format(""))  # Reserved (leave blank)
+        control.append("{:8.8}".format(self.journal_id.nacha_origination_dfi_identification))  # Originating DFI Identification
+        control.append("{:07d}".format(batch_nr))  # Batch Number
+
+        return "".join(control)

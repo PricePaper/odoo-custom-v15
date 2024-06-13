27d04b502ffc68f99ebd5d8f7d4eec6ccba8472a
harikrishnan <harikrishnan@confianzit.com>
2024-06-13 18:22:40 +0530

    [feat] Payment Acquirer API

--- ppt_mobile_apis/models/__init__.py
+++ ppt_mobile_apis/models/__init__.py
@@ -2,4 +2,5 @@ from . import partner
 from . import account
 from . import sale_order
 from . import push_notification
+from . import res_users
 
--- ppt_mobile_apis/models/account.py
+++ ppt_mobile_apis/models/account.py
@@ -7,3 +7,36 @@ class PaymentTerm(models.Model):
     _inherit = 'account.payment.term'
 
     payment_method = fields.Selection([('ach-debit', 'ACH-Debit')], string="Payment Method")
+
+
+
+class PaymentAcquirer(models.Model):
+    _inherit = 'payment.acquirer'
+
+    @api.model
+    def wrapper_fetch_acquirer(self, acquirer_id):
+
+        result = []
+        message = {'success': False,
+                   'error': False,
+                   'data': False}
+
+        if not isinstance(acquirer_id, int):
+            message['error'] = "acquirer_id must be an integer"
+            result.append(message)
+            return result
+
+        payment_acquirer = self.browse(acquirer_id)
+
+        if not payment_acquirer.exists():
+            message['error'] = "Payment Acquirer does not exist"
+            result.append(message)
+            return result
+
+        message['data'] = payment_acquirer.sudo().read()
+        message['success'] = True
+
+        result.append(message)
+        return result
+
+

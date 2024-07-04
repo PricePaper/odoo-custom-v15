5e355984c7d5cf48634652e1f1bcadb604a8818b
pauljose456 <paul.jose@confianzit.biz>
2024-07-04 16:13:24 +0530

    get price permission for portal user

--- website_base/models/partner.py
+++ website_base/models/partner.py
@@ -6,17 +6,17 @@ class ResPartner(models.Model):
     _inherit='res.partner'
 
     def get_product_price(self,product_ids):
-           
+
         """
         Fetch unit price from the relevant pricelist of partner
         if product is not found in any pricelis_uot,set
         product price as Standard price of product
         """
         prices={}
-        
+
         for product_id in product_ids:
 
-            partner_com = self
+            partner_com = self.sudo()
             product_id  =self.env['product.product'].browse([int(product_id)])
             prices_all = self.env['customer.product.price']
             for rec in partner_com.customer_pricelist_ids:
@@ -34,14 +34,14 @@ class ResPartner(models.Model):
                 if price_rec.pricelist_id.type == 'customer' and not price_rec.partner_id and prices_all.filtered(lambda r: r.partner_id):
                     continue
 
-                
+
                 product_price = price_rec.price
                 price_dict  ={'uom':price_rec.product_uom.id,'price':float_round(product_price,precision_digits=2)}
                 uom_price.append(price_dict)
-                
-            
+
+
             if product_id :
-                
+
                 uom_price_main = product_id.uom_standard_prices
                 if uom_price_main:
                     standard_prices = [{'uom':r.uom_id.id,'price':float_round(r.price,precision_digits=2)} for r in  uom_price_main]
@@ -49,7 +49,5 @@ class ResPartner(models.Model):
                 msg = "Unit Price for this product is not found in any pricelists, fetching the unit price as product standard price."
 
             prices[product_id.id] = [{"customer_prices":uom_price,"standart_prices":standard_prices}]
-        
+
         return prices
-
-

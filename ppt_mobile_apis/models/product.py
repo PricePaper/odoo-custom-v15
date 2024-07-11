96d814845d22cfcb585b91227e3aa91546d2a7a3
pauljose456 <paul.jose@confianzit.biz>
2024-07-11 10:42:12 +0530

    MAPP product quantity_available

--- ppt_mobile_apis/models/product.py
+++ ppt_mobile_apis/models/product.py
@@ -5,11 +5,12 @@ from odoo import api, models, fields
 class Product(models.Model):
     _inherit = 'product.product'
 
+    portal_qty_available = fields.Float(
+        'Portal qty On Hand', compute='_compute_portal_quantity',
+        digits='Product Unit of Measure')
 
-    def get_product_qty_available(self):
 
-        product_qty = []
+    def _compute_portal_quantity(self):
+        
         for product in self:
-            product = product.sudo()
-            product_qty.append({'product_id': product.id, 'quantity': product.quantity_available})
-        return product_qty
+            product.portal_qty_available = product.sudo().quantity_available

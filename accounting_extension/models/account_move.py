0b14bd809a9ec3a3ec96975c2b22fce58c2d4de6
Deviprasad V <deviprasad@confianzit.biz>
2024-07-30 22:24:18 +0530

    for debugging

--- accounting_extension/models/account_move.py
+++ accounting_extension/models/account_move.py
@@ -188,6 +188,40 @@ class AccountMove(models.Model):
             logging.error("Incoming vendor bill processing code by DP failed\n The error is %s" % e)
         return move
 
+    def _check_balanced(self):
+        ''' Assert the move is fully balanced debit = credit.
+        An error is raised if it's not the case.
+        '''
+        moves = self.filtered(lambda move: move.line_ids)
+        if not moves:
+            return
+
+        # /!\ As this method is called in create / write, we can't make the assumption the computed stored fields
+        # are already done. Then, this query MUST NOT depend of computed stored fields (e.g. balance).
+        # It happens as the ORM makes the create with the 'no_recompute' statement.
+        self.env['account.move.line'].flush(self.env['account.move.line']._fields)
+        self.env['account.move'].flush(['journal_id'])
+        self._cr.execute('''
+            SELECT line.move_id, ROUND(SUM(line.debit - line.credit), currency.decimal_places)
+            FROM account_move_line line
+            JOIN account_move move ON move.id = line.move_id
+            JOIN account_journal journal ON journal.id = move.journal_id
+            JOIN res_company company ON company.id = journal.company_id
+            JOIN res_currency currency ON currency.id = company.currency_id
+            WHERE line.move_id IN %s
+            GROUP BY line.move_id, currency.decimal_places
+            HAVING ROUND(SUM(line.debit - line.credit), currency.decimal_places) != 0.0;
+        ''', [tuple(self.ids)])
+
+        query_res = self._cr.fetchall()
+        if query_res:
+            ids = [res[0] for res in query_res]
+            sums = [res[1] for res in query_res]
+            logging.error(('***********',moves,moves.line_ids))
+            for l in moves.line_ids:
+                logging.error((l,l.account_id.name,l.name,l.debit,l.credit))
+            raise UserError(_("Cannot create unbalanced journal entry. Ids: %s\nDifferences debit - credit: %s") % (ids, sums))
+
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'

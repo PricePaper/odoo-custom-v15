d02ade6ab7db9c4d83c16b14a6ea0d75c2453c30
pauljose456 <paul.jose@confianzit.biz>
2024-08-21 14:14:50 +0530

    MAPP partner delivery date

--- ppt_mobile_apis/models/partner.py
+++ ppt_mobile_apis/models/partner.py
@@ -208,6 +208,7 @@ class Partner(models.Model):
 
     def get_partner_delivery_date(self):
         partner_deliver_date = []
+        self = self.sudo()
         for partner in self:
             shipping_date = date.today() + relativedelta(days=1)
             day_list = []
@@ -254,5 +255,5 @@ class Partner(models.Model):
                     day_diff = (6 - weekday) + day_list[0] + 1
                 shipping_date = date.today() + relativedelta(days=day_diff)
             shipping_date = shipping_date.strftime("%Y-%m-%d")
-            partner_deliver_date.append({'partner': partner.id, 'deliver_by': shipping_date})
+            partner_deliver_date.append({'partner': partner.id, 'deliver_by': shipping_date, 'day_list': day_list})
         return partner_deliver_date

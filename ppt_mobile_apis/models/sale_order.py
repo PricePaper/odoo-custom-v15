67d980c145f7842c058dde4e7ff3c4757fbd2cca
pauljose456 <paul.jose@confianzit.biz>
2024-08-22 11:46:41 +0530

    Mapp sale order Mapp unique ID

--- ppt_mobile_apis/models/sale_order.py
+++ ppt_mobile_apis/models/sale_order.py
@@ -6,6 +6,31 @@ from odoo import api, models, fields
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    mapp_record_id = fields.Char('Mapp Unique ID')
+
+    @api.model
+    def sale_order_create_write_wrapper(self, method, vals, record_id=False):
+        if method == 'create':
+            order = self.search([('mapp_record_id', '=', vals.get('mapp_record_id', ''))])
+            if order:
+                return True
+            else:
+                new_order = self.create(vals)
+                return new_order.id
+        elif method == 'write':
+            order = self.search([('id', '=', record_id)])
+            existing_lines = order.order_line.mapped('mapp_record_id')
+
+            for line in vals.get('order_line', []):
+                if line[0] == 0:
+                    if line[2].get('mapp_record_id', False):
+                        order_line = order.order_line.filtered(lambda r: r.mapp_record_id == line[2].get('mapp_record_id', False))
+                        if order_line:
+                            line[0] = 1
+                            line[1] = order_line.id
+            return order.write(vals)
+        return []
+
     def wrapper_sale_order_action_confirm(self):
         self.ensure_one()
         result = []
@@ -36,3 +61,8 @@ class SaleOrder(models.Model):
         if team:
             return {'team_id': int(team)}
         return {'team_id': False}
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    mapp_record_id = fields.Char('Mapp Unique ID')

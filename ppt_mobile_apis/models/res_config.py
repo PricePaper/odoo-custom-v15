3b9dcd57b8b547a382d4904121b2bedc9e8000ac
pauljose456 <paul.jose@confianzit.biz>
2024-09-12 09:38:28 +0530

    new arrival categ

--- ppt_mobile_apis/models/res_config.py
+++ ppt_mobile_apis/models/res_config.py
@@ -8,6 +8,9 @@ class ResConfigSettings(models.TransientModel):
     mobile_app_sale_team = fields.Many2one('crm.team',
         string='Mobile App Sale Team',
         config_parameter='ppt_mobile_apis.mobile_app_sale_team')
+    mobile_app_new_category = fields.Many2one('product.public.category',
+        string='Mobile App New Arrival category',
+        config_parameter='ppt_mobile_apis.mobile_app_new_category')
 
 
     cod_payment_term = fields.Many2one('account.payment.term',string='COD',config_parameter='ppt_mobile_apis.cod_payment_term')
